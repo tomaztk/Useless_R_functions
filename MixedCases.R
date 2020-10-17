b1a13f41f94f7ff1e655ca306c0508eb9efcda91
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-10-17 12:46:55 +0200

    Review overall logic of random vs. prev. 2 chars

--- MixedCases.R
+++ MixedCases.R
@@ -17,31 +17,20 @@ MixedCases <- function(stavek) {
   
   chars <- strsplit(stavek, "")[[1]]
   for (i in seq_along(chars)) {
-    if (i<=2) {
+    # if previous 2 characters have the same case, use the opposite 
+    if (i > 2 && all(grepl(is.upper, chars[i-seq_len(2)]))) {
+      chars[[i]] <- tolower(chars[[i]])
+    }
+    else if (i > 2 && all(grepl(is.lower, chars[i-seq_len(2)]))) {
+      chars[[i]] <- toupper(chars[[i]])
+    }
+    else {
       random_stevilka = sample(0:1, 1, replace=TRUE)
       if (random_stevilka == 0) {
         chars[[i]] <- toupper(chars[[i]])
       }
       else {
         chars[[i]] <- tolower(chars[[i]])
-      }
-    }
-    else {
-      # if previous 2 characters have the same case, use the opposite 
-      if (all(grepl(is.upper, chars[i-seq_len(2)]))) {
-        chars[[i]] <- tolower(chars[[i]])
-      }
-      else if (all(grepl(is.lower, chars[i-seq_len(2)]))) {
-        chars[[i]] <- toupper(chars[[i]])
-      }
-      else {
-        random_stevilka = sample(0:1, 1, replace=TRUE)
-        if (random_stevilka == 0) {
-          chars[[i]] <- toupper(chars[[i]])
-        }
-        else { 
-          chars[[i]] <- tolower(chars[[i]])
-        }
       }
     }
   }
