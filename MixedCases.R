07007004cbc3f4167243c5a97ef91186bf5384ed
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-10-17 12:17:20 +0200

    Consistent cosmetic spacing / indention

--- MixedCases.R
+++ MixedCases.R
@@ -11,49 +11,48 @@
 
 
 
-MixedCases <- function(stavek){
-nov_stavek = ""
-cifra = 0
-crka = ""
-is.upper <- "[A-Z]"
-is.lower <- "[a-z]"
-
-
-for (crka in strsplit(stavek, "")[[1]]){
-    if (nchar(nov_stavek)<2){
+MixedCases <- function(stavek) {
+  nov_stavek = ""
+  cifra = 0
+  crka = ""
+  is.upper <- "[A-Z]"
+  is.lower <- "[a-z]"
+  
+  
+  for (crka in strsplit(stavek, "")[[1]]) {
+    if (nchar(nov_stavek)<2) {
       random_stevilka = sample(0:1, 1, replace=TRUE)
-      if (random_stevilka == 0){
+      if (random_stevilka == 0) {
         nov_stavek = paste(nov_stavek,toupper(crka), sep = "")
       }
       else {
         nov_stavek = paste(nov_stavek,tolower(crka), sep = "")
       }
     }
-   else{
-     if(( grepl(pattern = is.upper,  x=(strsplit(nov_stavek,"")[[1]][(cifra-2)])) &  
-         grepl(pattern = is.upper,  x=(strsplit(nov_stavek,"")[[1]][(cifra-1)])) |  
-         
-         grepl(pattern = is.lower,  x=(strsplit(nov_stavek,"")[[1]][(cifra-2)])) &  
-         grepl(pattern = is.lower,  x=(strsplit(nov_stavek,"")[[1]][(cifra-1)]))   ) == TRUE){
-          if (  grepl(pattern = is.upper,  x=(strsplit(nov_stavek,"")[[1]][(cifra-1)])) ) {
-              nov_stavek = paste(nov_stavek, tolower(crka), sep = "")
-          }
-          else {
-           nov_stavek = paste(nov_stavek, toupper(crka), sep = "")
-          
-            }
-     }
-    else  {
-          random_stevilka = sample(0:1, 1, replace=TRUE)
-         if (random_stevilka == 0){
+    else {
+      if (( grepl(pattern = is.upper, x=(strsplit(nov_stavek,"")[[1]][(cifra-2)])) & 
+            grepl(pattern = is.upper, x=(strsplit(nov_stavek,"")[[1]][(cifra-1)])) | 
+            
+            grepl(pattern = is.lower, x=(strsplit(nov_stavek,"")[[1]][(cifra-2)])) & 
+            grepl(pattern = is.lower, x=(strsplit(nov_stavek,"")[[1]][(cifra-1)])) ) == TRUE) {
+        if ( grepl(pattern = is.upper, x=(strsplit(nov_stavek,"")[[1]][(cifra-1)])) ) {
+          nov_stavek = paste(nov_stavek, tolower(crka), sep = "")
+        }
+        else {
+          nov_stavek = paste(nov_stavek, toupper(crka), sep = "")
+        }
+      }
+      else {
+        random_stevilka = sample(0:1, 1, replace=TRUE)
+        if (random_stevilka == 0) {
           nov_stavek = paste(nov_stavek, toupper(crka), sep = "") 
-          }
+        }
         else { 
           nov_stavek = paste(nov_stavek, tolower(crka), sep = "") 
-          }
+        }
       }
-   }
-   #cifra = cifra + 1 
+    }
+    #cifra = cifra + 1 
   }
   return(nov_stavek)
 }
