2171628bcfc2b59b7e78c656fa151fc9107d7074
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-10-23 21:22:45 +0200

    script update

--- RJobTitleGenerator.R
+++ RJobTitleGenerator.R
@@ -4,7 +4,7 @@
 # Series:
 # Little Useless-useful R functions #3
 # Created: October 21, 2020
-# Author: Tomaû Kaötrun
+# Author: Toma≈æ Ka≈°trun
 # Blog: tomaztsql.wordpress.com
 # V.1.0
 
@@ -14,34 +14,35 @@
 RJobTitle <- function(){
 
 ### Values
-Roles = c('Analyst','Project Manager','Expert','Manager','Programmer','Artist'
-,'Tamer','Developer','Wrangler','DataFramer','Sherpa','Philosopher','ggPlotter'
-,'Data Manipulator','Evangelist','Hero','Guru','Slayer','Composer','Reader','Outliner'
-,'Proof-reader','Assistant','Operator','Coffee Maker','Pizza re-heater'
-,'Tester','Deep tester','Backward tester','Office hater','Hater','Warrior','Junkie'
-,'Wizard','Leader','King','Approver','Engineer','Architect','Rockstar','Ninja','R Coder'
-,'Python Hater','Ninja','Captain','Strategist','Consultant','Counsellor','Organizer'
-,'Endorser','Cow','Dog','Library Installer','Cheever','Lazy Lasagna Eater','Fanboy','Copy/Paster'
-,'Researcher','Shadower','Guerilla','Helper','Debugger','Data Scientists'
-,'Statistician','Coffee Addict','Tidyverser','Knitter'
-,'R-Studio dispatcher','Advanced Copy/paster','R-Bloggers subscriber','Markdown Writter' )
+ Fancystuff = c( 'Regional','Only the best','Insane','Qualitative','Virtuous','Senior'
+                  ,'Junior','In-House','Outsourced','Magnificent','Evolutionary','Customer orientated','Product'
+                  ,'Dynamic','Corporate Lead','Legacy','Investor','Direct','International','Over-seas','Internal','Human'
+                  ,'Creative','Volunteer','Lead','4 Stages of','Complete','Most Advanced','State of the art','Super high'
+                  ,'First Class','Powerful','Data','Head of','Master of','Chief of','Officer','Lead','Special')
+  
+ RStuff <- c('CRAN Lover','R Environment','userR! conference','R Package','Lattice','Graphics','Factors','Probability distribution'
+             ,'Sampling','Vectors and numbers','package dependencies','Set.seed','Visualization','Data manipulation','Machine Learning'
+             ,'Plot.ly','Shiny','Sys.getenv','Lubridate','NA','NaN','Environment history','R Script Editor','S3 Class'
+             ,'Box Plot','Infix Operator','Parametrization','Slow Script','Long running Script','R ggplot library','Statistical Models'
+             ,'%>% clause','R WHILE loop','Kronecker product','Matching operator','Integer division','dplyr and data.table' )
+ 
 
-RStuff <- c('CRAN Lover','R Environment','userR! conference','R Package','Lattice','Graphics','Factors','Probability distribution'
-,'Sampling','Vectors and numbers','package dependencies','Set.seed','Visualization','Data manipulation','Machine Learning'
-,'Plot.ly','Shiny','Sys.getenv','Lubridate','NA','NaN','Environment history','R Script Editor','S3 Class'
-,'Box Plot','Infix Operator','Parametrization','Slow Script','Long running Script','R ggplot library','Statistical Models'
-,'WHERE clause','WHILE loop','Kronecker product','Matching operator','Integer division','dplyr and data.table' )
-
-Fancystuff = c( 'Regional','Only the best','Insane','Qualitative','Virtuous','Senior'
-,'Junior','In-House','Outsourced','Magnificent','Evolutionary','Customer orientated','Product'
-,'Dynamic','Corporate Lead','Legacy','Investor','Direct','International','Over-seas','Internal','Human'
-,'Creative','Volunteer','Lead','4 Stages of','Complete','Most Advanced','State of the art','Super high'
-,'First Class','Powerful','Data','Head of','Master of','Chief of','Officer','Lead','Special')
-
+ Roles = c('Analyst','Project Manager','Expert','Manager','Programmer','Artist'
+           ,'Tamer','Developer','Wrangler','DataFramer','Statistician','Philosopher','ggPlotter'
+           ,'Data Manipulator','tEvangelist','Hero','Guru','partz professional','Composer','Reader','Outliner'
+           ,'Proof-reader','zoo Assistant','data.frame Operator','Matrix Maker','dpylr lover'
+           ,'Tester','Deep tester','Backward tester','Office hater','Hollister','Warrior','Junkie'
+           ,'Wizard','Leader','King','Github Approver','CARET Engineer','n1071e Architect','Rockstar','Ninja','R Coder'
+           ,'Python Hater','Ninja','Captain','Strategist','Consultant','Counsellor','Organizer'
+           ,'Emacs Endorser','Dog','Library Installer','Cheever','RStudio specialist','R Fanboy','Functionist'
+           ,'Researcher','Shadower','Variable lover','Knitter Helper','Debugger','Data Scientists'
+           ,'Statistician','Coffee Addict','Tidyverser','Knitter'
+           ,'R-Studio dispatcher','Advanced Copy/paster','R-Bloggers subscriber','Markdown Writter' )
+ 
 
 ### Ordering of subsets and generating R Job Title
 RTitle <- paste(sample(Fancystuff,1, replace=TRUE),sample(RStuff,1, replace=TRUE),sample(Roles,1, replace=TRUE), sep= " ")
-
+RTitle <- paste("Your Greatest of them greatest made-up R job title is: ", RTitle)
 return(RTitle)
 
 }
