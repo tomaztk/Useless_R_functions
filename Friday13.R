e9272b693b4c82b4f816978c6a5c158720ced155
tomaztk <tomaz.kastrun@gmail.com>
2020-10-26 22:27:03 +0100

    adding friday

---
+++ Friday13.R
@@ -0,0 +1,34 @@
+##########################################
+# 
+# Is if full moon yet?
+# Is it Friday 13th?
+# Series:
+# Little Useless-useful R functions #4
+# Created: October 26, 2020
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+###########################################
+
+
+
+# non-deterministic
+IsItFriday13 <- function(){
+  #da <- "2020-11-13"
+  da <- Sys.Date()
+  rn <- as.POSIXlt(da)$wday
+  d <- as.POSIXlt(da)$mday 
+  
+  if (rn == 5 & d == 13){
+      print("true Fri-13")
+  } else {
+      print("naaah!")
+    }
+  }  
+
+
+IsItFriday13()
+
+
