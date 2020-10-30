f47dfd0a10b3a7c1c0b891a28ed50a049756e140
tomaztk <tomaz.kastrun@gmail.com>
2020-10-30 22:04:25 +0100

    added Lorem

---
+++ LoremIpsum.R
@@ -0,0 +1,31 @@
+##########################################
+# 
+# Lorem Ipsum generator for R
+# Series:
+# Little Useless-useful R functions #6
+# Created: October 30, 2020
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+###########################################
+
+
+function_list <- function(){
+  
+  lw <- builtins(internal = FALSE)
+  lw2 <- help(package="base") 
+}
+
+
+RLoremIpsum <- function(text_length){
+  
+  lw <- function_list
+  
+  
+}
+
+
+
+
