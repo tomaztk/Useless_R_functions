300786012a77bc73f5a411c109d188dcc13f2521
tomaztk <tomaz.kastrun@gmail.com>
2020-10-30 22:15:30 +0100

    added Lorem

--- LoremIpsum.R
+++ LoremIpsum.R
@@ -14,15 +14,21 @@
 
 function_list <- function(){
   
-  lw <- builtins(internal = FALSE)
-  lw2 <- help(package="base") 
-}
+  lw <- builtins() #(internal = FALSE)
+  #lw2 <- help(package="base") 
+  lw3 <- ls("package:base")
+  lwA <- c(lw,lw3)
+  lwA <- unique(lwA)
+  gsub("[[:punct:]]", " ", lwA)
+  return(lwA)
+  }
+
+function_list()
 
 
 RLoremIpsum <- function(text_length){
   
-  lw <- function_list
-  
+  lw <- function_list()
   
 }
 
