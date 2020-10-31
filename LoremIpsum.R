aa750170d0e8165ef1ca6cefa869de843a717a10
tomaztk <tomaz.kastrun@gmail.com>
2020-10-31 20:08:07 +0100

    adding changes

--- LoremIpsum.R
+++ LoremIpsum.R
@@ -3,7 +3,7 @@
 # Lorem Ipsum generator for R
 # Series:
 # Little Useless-useful R functions #6
-# Created: October 30, 2020
+# Created: October 31, 2020
 # Author: Tomaž Kaštrun
 # Blog: tomaztsql.wordpress.com
 # V.1.0
@@ -17,45 +17,44 @@
 function_list <- function(){
   
   lw <- builtins() #(internal = FALSE)
-  #lw2 <- help(package="base") 
+  lw2 <- help(package="base") 
   lw3 <- ls("package:base")
-  lwA <- c(lw,lw3)
+  lwA <- c(lw,lw2,lw3)
   lwA <- unique(lwA)
   lwA <- trimws(gsub("[[:punct:]]", " ", lwA))
   #ltrim / rtrim
-
   return(lwA)
   }
 
 
-#function_list()
 
-
-RLoremIpsum <- function(text_length){
+RLoremIpsum <- function(text_length, approx=TRUE){
   
   lw <- function_list()
-  text_length <- 1000
-  a <- do.call(paste0, replicate(1, sample(lw, text_length, TRUE), FALSE))
-  a <- paste0(" ", a, sep = " ", collapse = "")
-  a <- gsub("^ *|(?<= ) | *$", "", a, perl = TRUE)
-
+  LorIps <- ''
+  while (nchar(LorIps) < text_length) {
+    lw <- gsub("^ *|(?<= ) | *$", "", lw, perl = TRUE)
+    new_w <-  sample(lw,1, replace=TRUE)
+    LorIps <- paste(LorIps, new_w, sep = " ")
+    if (approx==FALSE){
+    LorIps <- substr(LorIps, 1, text_length)
+    }
+  }
+  
   #check last word - if cut, replace the last word (from last spacebar)
-  a <- substr(a, 1, text_length)
-  return(a)
+  last_word <- tail(strsplit(LorIps ,split=" ")[[1]],1)
+  #if ((last_word %in% lw) == FALSE) {
+    if ((nchar(last_word) == 1) == TRUE) {
+    LorIps <- substr(LorIps, 1, nchar(LorIps)-1) # replace last char with blank space
+    }
+   #len <- nchar(last_word)
+   #sapply(strsplit(lw, ","), nchar)  #== len
+  #}
+  return(LorIps)
 }
 
 
+
 # generated Lorem Ipsum with 1000 characters
-RLoremIpsum(1000)
+RLoremIpsum(10000, approx=TRUE)
 
-a
-
-
-last_word <- tail(strsplit(a ,split=" ")[[1]],1)
-if last_word exist in lw (exit)
-if (nchar(last_word) == 1) { gsub(a, tail(strsplit(a ,split=" ")[[1]],1), " ") } #if last word je 1 znak, replace s presledkom
-if (nchar(last_word) <> 1) { ll <- nchar(last_word); re.findall(r'\b[a-zA-Z]{4}\b', lw)  } # je razrezana beseda, ki jo zamenjaš z drugo iz nabora
-
-
-lw
-length(gregexpr("\\W+", lw)) + 1 
