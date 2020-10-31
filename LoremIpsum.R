f86c3a99547e9ffda5ac9b552024575e1599f7e1
tomaztk <tomaz.kastrun@gmail.com>
2020-10-31 08:54:29 +0100

    manjse spremembe

--- LoremIpsum.R
+++ LoremIpsum.R
@@ -34,9 +34,12 @@ function_list <- function(){
 RLoremIpsum <- function(text_length){
   
   lw <- function_list()
+  text_length <- 1000
   a <- do.call(paste0, replicate(1, sample(lw, text_length, TRUE), FALSE))
   a <- paste0(" ", a, sep = " ", collapse = "")
   a <- gsub("^ *|(?<= ) | *$", "", a, perl = TRUE)
+
+  #check last word - if cut, replace the last word (from last spacebar)
   a <- substr(a, 1, text_length)
   return(a)
 }
@@ -44,3 +47,15 @@ RLoremIpsum <- function(text_length){
 
 # generated Lorem Ipsum with 1000 characters
 RLoremIpsum(1000)
+
+a
+
+
+last_word <- tail(strsplit(a ,split=" ")[[1]],1)
+if last_word exist in lw (exit)
+if (nchar(last_word) == 1) { gsub(a, tail(strsplit(a ,split=" ")[[1]],1), " ") } #if last word je 1 znak, replace s presledkom
+if (nchar(last_word) <> 1) { ll <- nchar(last_word); re.findall(r'\b[a-zA-Z]{4}\b', lw)  } # je razrezana beseda, ki jo zamenjaš z drugo iz nabora
+
+
+lw
+length(gregexpr("\\W+", lw)) + 1 
