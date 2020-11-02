227ca914cb0ff455cfd07e235723218b360cb124
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-02 06:03:52 +0100

    minor changes

--- DrawFunctionCodeOnConsole.R
+++ DrawFunctionCodeOnConsole.R
@@ -12,15 +12,30 @@
 ###########################################
 
 
-
+# random text, later to be coode
 tkt <- "to je tole, pa takole je, ker tako je in sicer ne bi tako bilo, ker tako je in pika. Tako gre, ker tako je in sicer ne more tako, ker je kot ni in pika."
 
 r <- nchar(tkt)
 s <- ceiling(sqrt(r))
 
-bl <- "#############"
 
-draw_rect <- funtion(i,i)
-for (i in 1:s){
-   print( paste0("#",substr(tkt,i*s+1, i*s+s),"#" ))
+draw_rect <- function(size, codetext) {
+for (i in 1:size){
+   print( paste0("#",substr(codetext,i*size+1, i*size+size),"#" ))
+   }
+ }
+
+#test ....
+draw_rect(15,tkt)
+
+
+
+
+draw_shape <- function(fun, size, codetext){
+  
+  fun <- c("cos", "sin", "x", "x2", "x3", "xn")
+  
+  
 }
+
+
