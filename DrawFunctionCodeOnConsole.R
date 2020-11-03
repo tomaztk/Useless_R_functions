33d062ef27e3c86a2fd26c3e421985fc42efd9ca
tomaztk <tomaz.kastrun@gmail.com>
2020-11-03 06:20:03 +0100

    new funct added"

--- DrawFunctionCodeOnConsole.R
+++ DrawFunctionCodeOnConsole.R
@@ -3,8 +3,8 @@
 # Draw function on console plot with code
 # Series:
 # Little Useless-useful R functions #2
-# Created: November 1, 2020
-# Author: Tomaž Kaštrun
+# Created: November 1, 2020 - work in prog
+# Author: Tomaz Kastrun
 # Blog: tomaztsql.wordpress.com
 # V.1.0
 
@@ -15,18 +15,54 @@
 # random text, later to be coode
 tkt <- "to je tole, pa takole je, ker tako je in sicer ne bi tako bilo, ker tako je in pika. Tako gre, ker tako je in sicer ne more tako, ker je kot ni in pika."
 
-r <- nchar(tkt)
-s <- ceiling(sqrt(r))
 
+
+##### 1. Rectangle
 
 draw_rect <- function(size, codetext) {
-for (i in 1:size){
-   print( paste0("#",substr(codetext,i*size+1, i*size+size),"#" ))
-   }
- }
+  r <- nchar(codetext)
+  #s <- ceiling(sqrt(r))
+  x = size
+  y = floor(r/x)
+  for (i in 1:y){
+    if (i == 1){
+      cat( paste0("#",substr(codetext,1, size),"#","\n" ))
+    } else {
+      cat( paste0("#",substr(codetext,1+(size*i), size+(size*i)),"#","\n" ))
+    }
+  }
+}
 
-#test ....
-draw_rect(15,tkt)
+#test
+draw_rect(10,tkt)
+
+
+
+##### 1. Parallelogram
+draw_parallel <- function(h, codetext){
+  r <- nchar(codetext)
+  a = ceiling(sqrt(r*4/1.73))
+  v = ceiling(r/a) + h 
+  for (i in 1:v){
+    if (i == 1){
+      cat( paste0(paste0(replicate(i,"\n "), collapse=""),substr(codetext,1, a),paste0(replicate(i,"\n "), collapse="")))
+    } else {
+      cat( paste0(paste0(replicate(i," "), collapse=""),substr(codetext,1+(a*i)-i, a+(a*i)-i),paste0(replicate(i," "), collapse=""),"\n" ))
+    }
+  }
+}
+
+
+#test
+draw_parallel(10,tkt)
+
+
+
+
+
+draw_triang <- function(v, codetext){
+  
+}
 
 
 
@@ -34,7 +70,6 @@ draw_rect(15,tkt)
 draw_shape <- function(fun, size, codetext){
   
   fun <- c("cos", "sin", "x", "x2", "x3", "xn")
-  
   
 }
 
