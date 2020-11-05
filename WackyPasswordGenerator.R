c489e67a9783177981edfc8741822325e0dd4907
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-05 18:42:41 +0100

    changes

--- WackyPasswordGenerator.R
+++ WackyPasswordGenerator.R
@@ -14,22 +14,34 @@
 
 WackyPassword <- function(WP_length, WP_level=TRUE){
 
+  WP_length <- 20
   charblock1 = c(176:178, 185: 188, 200:206)
   charblock2 = c(73,105,108,124,49,33)
   numberblock3 <- sample(0:9, length(5),replace = TRUE)
+  
+
+   if (WP_level==TRUE){
  
-  if (WP_level==TRUE){
-    
-    werwe
+     
+     stopifnot(is.numeric(WP_length),  WP_length   > 0L,
+               is.character(charblock1), charblock1  > 0L, charblock1 <= WP_length,
+               is.numeric(charblock2), charblock2  > 0L, charblock2 <= WP_length,
+               is.numeric(numberblock3), numberblock3  > 0L, numberblock3 <= WP_length,
+               length(intersect(charblock1, charblock2, numberblock3)) == 0L)
+        
+     wacky <- charblock1
+     pass[capitals] <- uc
+     pass[numbers]  <- num
+     
+     paste0(pass, collapse = "")
+     
   }
   if (WP_level==FALSE){
     
     rerw
   }
   
-   
   data.frame(coderange,as.raw(coderange),row.names=rawToChar(as.raw(coderange),multiple=TRUE))
-  
   
 }
 
