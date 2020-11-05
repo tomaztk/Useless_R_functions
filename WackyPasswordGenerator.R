7e9cb5fec7682283e3ed688aaf024ceef7bada6c
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-05 21:35:29 +0100

    update

--- WackyPasswordGenerator.R
+++ WackyPasswordGenerator.R
@@ -13,7 +13,7 @@
 
 Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 
-
+# Running on Linux/MacOS
 WackyPassword <- function(WP_length){
   #charblock1 = c(176:178, 185: 188, 200:206)
   charblock1 <- c("\u2591","\u2592","\u2593")
@@ -22,27 +22,24 @@ WackyPassword <- function(WP_length){
   
   pass = ""
   Encoding(pass) <- "UTF-8"
-  
+  ran2 <- floor(sample(1:WP_length/2))
+  ran1 <- floor(sample(1:WP_length/2))
       while (nchar(pass) <= WP_length) {
         res2 <- sample(charblock2, 100,replace = TRUE)
         res2 <- rawToChar(as.raw(res2))
         Encoding(res2) <- "UTF-8"
-       # cat(res2)
-        pass <- paste0(pass,substr(res2,1,5),collapse="", sep= "")
+        start2 <- sample(1:90,1)
+        pass <- paste0(pass,substr(res2,start2,start2+ran2),collapse="", sep= "")
         
         
         res1 <- sample(charblock1, 100,replace = TRUE)
-        #res1 <- (rawToChar(as.raw(res1)))
-        #xx <- iconv(res1, "raw", "UTF-8")
-        #res1 <- (as.raw(res1))
-        #enc2utf8(res1)
         Encoding(res1) <- "UTF-8"
+        start1 <- sample(20:70,1)
         res <- paste0(res1, sep = "", collapse = "")
-        # cat(res1)
-        pass <- paste0(pass,substr(res,1,5), sep="", collapse = "")
+        pass <- paste0(pass,substr(res,start1,start1+ran1), sep="", collapse = "")
           }     
 
-  cat(eval(pass))
+  cat(eval(substr(pass,1,WP_length)))
 }
 
 
