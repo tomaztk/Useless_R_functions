cdb8c052084e7f731b6ae9e15c80ee2428f77e21
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-05 07:43:34 +0100

    adding file

---
+++ WackyPasswordGenerator.R
@@ -0,0 +1,38 @@
+##########################################
+# 
+# Generate wacky (strong) password
+# Series:
+# Little Useless-useful R functions #8
+# Created: November 5, 2020 - work in prog
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+###########################################
+
+
+WackyPassword <- function(WP_length, WP_level=TRUE){
+
+  charblock1 = c(176:178, 185: 188, 200:206)
+  charblock2 = c(73,105,108,124,49,33)
+  numberblock3 <- sample(0:9, length(5),replace = TRUE)
+ 
+  if (WP_level==TRUE){
+    
+    werwe
+  }
+  if (WP_level==FALSE){
+    
+    rerw
+  }
+  
+   
+  data.frame(coderange,as.raw(coderange),row.names=rawToChar(as.raw(coderange),multiple=TRUE))
+  
+  
+}
+
+
+
+
