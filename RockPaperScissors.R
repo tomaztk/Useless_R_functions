51a99873775e0aa53f74074a146a6aa4a116a767
tomaztk <tomaz.kastrun@gmail.com>
2020-11-12 19:01:20 +0100

    adding function

--- RockPaperScissors.R
+++ RockPaperScissors.R
@@ -13,15 +13,33 @@
 ###########################################
 
 
-##### Input bet
 
-RPS <- function(bet = c("r","p","s")) {
-  
-  players <- c ("me", "player2", "player3")
-  print(bet)
+
+##### Input bet as a function
+play_RPS <- function(bet) {
+   if(bet %in% c("R","P", "S")){
+     
+  solution_df <- data.frame(combo=c("RP", "PR", "PS", "SP", "RS", "SR", "PP", "RR", "SS"), win = c("01","10", "01","10", "10", "01", "00","00","00") )
+  REngine <- sample(bets,1)  
+  combo <- paste0(REngine,me, collapse="")
+  res <-solution_df[ which(solution_df$combo==combo),2]
+  if (res=="10"){
+    print("You lose")
+    print(paste0("Computer draw: ", REngine), collapse="")
+  } else if(res=="00"){
+    print("It's a tie")
+    print(paste0("Computer draw: ", REngine), collapse="")
+    }else {
+    print("You win!")
+    print(paste0("Computer draw: ", REngine), collapse="")
+    }
+   }
+  else {
+    print("Please input valid bet")
+  }
 }
 
 
-RPS("g")
+play_RPS("R")
 
  
