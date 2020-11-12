6b15070f2d823ea3626c48a030137427de9b4eda
tomaztk <tomaz.kastrun@gmail.com>
2020-11-12 18:20:14 +0100

    adding file

---
+++ RockPaperScissors.R
@@ -0,0 +1,27 @@
+##########################################
+# 
+# Play Rock-Paper-Scissors with R
+# Series:
+# Little Useless-useful R functions #8
+# Created: November 11, 2020
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+#        - adding rocks
+###########################################
+
+
+##### Input bet
+
+RPS <- function(bet = c("r","p","s")) {
+  
+  players <- c ("me", "player2", "player3")
+  print(bet)
+}
+
+
+RPS("g")
+
+ 
