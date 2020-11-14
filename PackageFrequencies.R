a7e6f601791644b60c9a36ff2d6e3a96c2d5787c
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-14 22:39:18 +0100

    uselessrecarmact

---
+++ PackageFrequencies.R
@@ -0,0 +1,35 @@
+##########################################
+# 
+# Frequency of words in vignettes / libraries
+# Series:
+# Little Useless-useful R functions #8
+# Created: November 11, 2020
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: - 
+###########################################
+
+packs <- c("ggplot2", "Hmisc")
+
+require("ggplot2")
+require("Hmisc")
+
+
+rr <<- help(package="MASS")
+rr <<-browseVignettes(package="ggplot2")
+rr <<-vignette(package="ggplot2") 
+rr <<-demo(package="ggplot2")
+
+
+# good
+help.search("^glm")
+
+# get all functions per package
+fx <- ls("package:ggplot2")
+
+# get content for each function per package
+w2 <- help("geom_label",package="ggplot2") 
+
+ls(w2)
