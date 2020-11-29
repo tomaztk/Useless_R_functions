5af66888a222e4321ec23dabb972603087ecf186
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-11-29 09:14:21 +0100

    drawLetter function

---
+++ functions/DrawLetter.R
@@ -0,0 +1,13 @@
+##################################################
+# Draw a letter :D
+#
+# call function draw_letter("A") and get output:
+#      AA
+#     AAAA
+#    AA  AA
+#   AA    AA
+#  AAAAAAAAAA
+# AA        AA
+#AA          AA
+##################################################
+
