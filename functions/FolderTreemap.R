45750afbd116c657c00ae484c831065d695c9c1b
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2020-12-30 23:01:31 +0100

    adding chngs

--- functions/FolderTreemap.R
+++ functions/FolderTreemap.R
@@ -22,26 +22,12 @@ FolderTreemap <- function(directory){
   dirs <- aa[file.info(aa)$isdir]
   for (i in 1:length(dirs)){
     name_f <- basename(dirs[i])
-    #utils:::format.object_size(sum(file.info(list.files(path =input_directory, recursive = T, full.names = T))$size), "auto")
     size_f <- sum(file.info(list.files(path=dirs[i], recursive = T, full.names = T))$size)
     df_temp <- data.frame(size=size_f/(1024*1024), folder=name_f)
     df <- rbind(df, df_temp)
   }
-  df_out <<- df
-  totalSize <- as.integer(sum(df_out$size))
-  
-  # plot(
-  #   x = df$folder,
-  #   y = df$size, 
-  #   #x = df$size,
-  #   type="h",
-  #   main="Size of the subfolders",
-  #   ylab="Size (in Mega-bytes)",
-  #   border = "dark blue", 
-  #   col = "light blue",
-  #   labels = TRUE)
-  
-  p <- treemap(df_out,
+  totalSize <- as.integer(sum(df$size))
+  p <- treemap(df,
                index=c("folder"),
                vSize="size",
                type="index",
@@ -53,7 +39,6 @@ FolderTreemap <- function(directory){
                  c("right", "bottom")
                )  
   )            
-  
 }
 
 input_directory <- "/users/tomazkastrun/Documents/Github"
