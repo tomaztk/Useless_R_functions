ed8320a1ca65422668599678c240caf62e5c07c6
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-03 22:06:59 +0100

    useless R_Py fun

--- functions/R_dataFrame_to_Python_Dataframe.R
+++ functions/R_dataFrame_to_Python_Dataframe.R
@@ -1,40 +1,73 @@
-## Export R Data.frame to  R command ###Python Pandas
+##########################################
+# 
+# Export R Data.frame schema and data to  
+# external file for Python Pandas
+# 
+# Series:
+# Little Useless-useful R functions #15
+# Created: January  04, 2021
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+# Changelog: 
+#  
+###########################################
 
 
 ## goal: Python Dictionary
 ## d = {'col1': [1, 2], 'col2': [3, 4]}
-## df = pd.DataFrame(data=d)
-##
-
-iris <- data.frame(iris[1:8,])
-Nn <- names(iris)
-nr <- nrow(iris)
 
 
-sapply(iris[5], class)
-
-py_df <- "d = {"
-
-for (x in 1:length(Nn)){
-    var <- (Nn[x])
-    py_df <- paste0(py_df, "'",var,"':[", collapse=NULL)
+RtoPy <- function(df_input, filename_path) {
+  
+    # column names and number of rows
+    Nn <- names(df_input)
+    nr <- nrow(df_input)
+    
+#Python is Indentation  sensitive - leave these two lines without indentation   
+py_df <- "import pandas as pd
+d = {"
+    
+    for (x in 1:length(Nn)){
+      var <- (Nn[x])
+      #Column Names
+      py_df <- paste0(py_df, "'",var,"':[", collapse=NULL)
+      
+      #Data Rows
       for (i in 1:nr) {
-        val <- iris[i,x]
-        if (sapply(iris[i,x], class) == "factor") {
+        val <- df_input[i,x]
+        #Check for data type
+        if (sapply(df_input[i,x], class) == "factor") {
           py_df <- paste0(py_df, "'",val,"'", ",", collapse=NULL)
+          #for last value in a column
+          if (i == nr){
+            py_df <- paste0(py_df, "'",val,"'", "],","\n", collapse=NULL)
+          } 
         } else {
           py_df <- paste0(py_df, val, ",", collapse=NULL)
+          #for last value in a column
+          if (i == nr){
+            py_df <- paste0(py_df, val, "],","\n", collapse=NULL)
+          }  
         }
-        if (i == nr){
-          py_df <- paste0(py_df, val, "],", collapse=NULL)
-        }
+        
       }
-    if (x == length(Nn)){
-      py_df <- paste0(py_df, "}
+      if (x == length(Nn)){
+        py_df <- substr(py_df, 1, nchar(py_df)-2)
+        py_df <- paste0(py_df, "}
 df=pd.DataFrame(data=d)", collapse=NULL)
-  }
+      }
+    }
+    
+    ## Store to file
+    sink(file = filename_path)
+    cat(py_df)
+    sink(file = NULL)
 }
 
-print(py_df)
 
 
+# Get the data from R data.frame to Python Pandas script
+iris <- iris
+iris <- data.frame(iris[1:15,]) 
+RtoPy(iris, "/users/tomazkastrun/desktop/iris_py.txt")
