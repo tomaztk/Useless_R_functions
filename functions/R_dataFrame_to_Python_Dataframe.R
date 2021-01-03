bebe4e6b9b6f4d139ca25b116a79916f8e76c487
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-03 21:28:52 +0100

    useless R_Py fun

--- functions/R_dataFrame_to_Python_Dataframe.R
+++ functions/R_dataFrame_to_Python_Dataframe.R
@@ -13,25 +13,25 @@ nr <- nrow(iris)
 
 sapply(iris[5], class)
 
-py_df <- 
-"import pandas as pd
-d = {"
+py_df <- "d = {"
 
 for (x in 1:length(Nn)){
     var <- (Nn[x])
     py_df <- paste0(py_df, "'",var,"':[", collapse=NULL)
       for (i in 1:nr) {
-      #print(iris[i,x])
-      val <- iris[i,x]
-      if( apply(iris[5], class) == "factor" || sapply(iris[5], class) == "character")
-      py_df <- paste0(py_df, val, ",", collapse=NULL)
-      if (i == nr){
-        py_df <- paste0(py_df, val, "],", collapse=NULL)
-      }
+        val <- iris[i,x]
+        if (sapply(iris[i,x], class) == "factor") {
+          py_df <- paste0(py_df, "'",val,"'", ",", collapse=NULL)
+        } else {
+          py_df <- paste0(py_df, val, ",", collapse=NULL)
+        }
+        if (i == nr){
+          py_df <- paste0(py_df, val, "],", collapse=NULL)
+        }
       }
     if (x == length(Nn)){
       py_df <- paste0(py_df, "}
-                      df=pd.DataFrame(data=d)", collapse=NULL)
+df=pd.DataFrame(data=d)", collapse=NULL)
   }
 }
 
