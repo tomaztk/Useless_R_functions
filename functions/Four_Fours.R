00e43cbc68e0189ad43904b2d9d14e13f3759c4f
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-05 06:47:23 +0100

    adding 44

--- functions/Four_Fours.R
+++ functions/Four_Fours.R
@@ -12,47 +12,58 @@
 #  
 ###########################################
 
-
-
-oper <- c("+","*", "-", "/")
-para <- c("(",")")
-
-#i <- 0
-for (i in 0:2) {
-    step_counter <- 0
-    res <- i + 1
-      while (i != res) {
-      oper3 <- sample(oper,4,replace=FALSE)
-      for44 <- paste0("4",oper3[1],"4",oper3[2],"4",oper3[3],"4")
-      
-      #adding paranthesis
-      # dif =  3, 5, 7 
-      stopit <- FALSE
-      while (!stopit){
-        pos_par <<- sort(sample(1:7,2)) 
-        nn <- pos_par[1]
-        mm <- pos_par[2]
-        rr <<- abs(nn-mm)
+# Four Fours function
+four_fours <- function(maxnum) {
+  for (i in 0:maxnum) {
+      oper <- c("+","*", "-", "/")
+      para <- c("(",")")
+      step_counter <- 0
+      res <- i + 1
+        while (i != res) {
+        oper3 <- sample(oper,4,replace=TRUE)
+        for44 <- paste0("4",oper3[1],"4",oper3[2],"4",oper3[3],"4")
         
-        if (rr == 4 | rr == 5 ){
-          stopit <- TRUE
+        #adding paranthesis
+        # dif =  3, 5, 7 
+        stopit <- FALSE
+        while (!stopit){
+          pos_par <<- sort(sample(1:7,2)) 
+          nn <- pos_par[1]
+          mm <- pos_par[2]
+          rr <<- abs(nn-mm)
           
+          if (rr == 4 | rr == 5 ){
+            stopit <- TRUE
+            
+          }
         }
-      }
+        
+        for44 <- paste0(substr(for44, 1, nn-1), "(", substr(for44, nn, nchar(for44)), sep = "")
+        for44 <- paste0(substr(for44, 1, mm-1+1), ")", substr(for44, mm+1, nchar(for44)), sep = "")
+        
+       # if (for44 ) like "(/" or "(-" or "(*" or "(+" -> switch to -> "/(" or "-("
+        for44 <- gsub("\\(-", "-\\(", for44)
+        for44 <- gsub("\\(/", "/\\(", for44)
+        for44 <- gsub("(*", "*(", for44, fixed=TRUE)
+        for44 <- gsub("(+", "+(", for44, fixed=TRUE)
+        for44 <- gsub("\\+)", "\\)+", for44)
+        for44 <- gsub("\\-)", "\\)-", for44)
+        for44 <- gsub("\\*)", "\\)*", for44)
+        for44 <- gsub("\\/)", "\\)/", for44)
+        
+        
+        res <- eval(parse(text=for44))
+        #print(paste0("vrednost: ",i,". formula: ", for44, ". rezultat: ",res ,collapse=NULL))
+        step_counter <- step_counter + 1
+        if (res==i){
+          print(paste0("Value: ", res, " was found formula: ", for44, " with result: ", res, " and steps: ", step_counter, collapse=NULL))
+        }
       
-      for44 <- paste0(substr(for44, 1, nn-1), "(", substr(for44, nn, nchar(for44)), sep = "")
-      for44 <- paste0(substr(for44, 1, mm-1+1), ")", substr(for44, mm+1, nchar(for44)), sep = "")
-      
-     # if (for44 ) like "(/" or "(-" or "(*" or "(+" -> switch to -> "/(" or "-("
-      
-      res <- eval(parse(text=for44))
-      #print(paste0("vrednost: ",i,". formula: ", for44, ". rezultat: ",res ,collapse=NULL))
-      step_counter <- step_counter + 1
-      if (res==i){
-        print(paste0("Value: ", res, " was found formula: ", for44, " with result: ", res, " and steps: ", step_counter, collapse=NULL))
-      }
-    
-      }
-    i <- i + 1
+        }
+      i <- i + 1
     }
 
+}
+
+#Run function
+four_fours(6)
