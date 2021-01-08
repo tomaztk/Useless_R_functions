463f6b03a5acb88396908bd916af57512fa28571
tomaztk <tomaz.kastrun@gmail.com>
2021-01-08 06:05:24 +0100

    adding Number Countdown

--- functions/NumberCountdownGame.R
+++ functions/NumberCountdownGame.R
@@ -49,3 +49,79 @@ while (ran_3digit_num != res) {
   
   
 }
+
+
+solve_inner <- function(nums, value, expr = value, target, verbose = FALSE) {
+  
+  if (value == target) {
+    if (verbose) {
+      cat(expr, "\n")
+    }
+    return(expr)
+  } else if (length(nums) == 0L) {
+    return()
+  }
+  
+  all_res <- c()
+  
+  for (op in c('+', '-', '*', '/')) {
+    for(num_idx in seq_along(nums)) {
+      
+      new_value <- switch(
+        op,
+        '+' = nums[num_idx] + value,
+        '-' = nums[num_idx] - value,
+        '*' = nums[num_idx] * value,
+        '/' = ifelse(nums[num_idx] %% value == 0L, nums[num_idx] %/% value, NA_integer_)
+      )
+      
+      if (!is.na(new_value)) {
+        res <- solve_inner(
+          nums    = nums[-num_idx], 
+          value   = new_value, 
+          expr    = paste("(", nums[num_idx], ' ', op, ' ', expr, ")", sep = ''),
+          target  = target,
+          verbose = verbose
+        )
+        if (!is.null(res)) { all_res <- c(all_res, res) }
+      }    
+      
+      if (op %in% c('-', '/')) {
+        new_value <- switch(
+          op,
+          '-' = value - nums[num_idx],
+          '/' = ifelse(value && nums[num_idx] == 0L, value %/% nums[num_idx], NA_integer_)
+        )
+        
+        if (!is.na(new_value)) {
+          res <- solve_inner(
+            nums    = nums[-num_idx], 
+            value   = new_value, 
+            expr    = paste("(", expr, ' ', op, ' ', nums[num_idx], ")", sep = ''),
+            target  = target,
+            verbose = verbose
+          )
+          if (!is.null(res)) { all_res <- c(all_res, res) }
+        }    
+      }
+    } # num_idx loop
+  }   # op loop
+  
+  all_res 
+}
+
+
+
+solve_countdown <- function(nums, target, verbose = FALSE) {
+  nums   <- as.integer(nums)
+  target <- as.integer(target)
+  seq_along(nums) %>% 
+    purrr::map(~solve_inner(nums[-.x], value = nums[.x], target = target, verbose = verbose)) %>%
+    purrr::flatten_chr() %>%
+    unique()
+}
+
+
+nums   <- c(50, 9, 4, 5, 9, 3)
+target <- 952 
+solve_countdown(nums, target)
