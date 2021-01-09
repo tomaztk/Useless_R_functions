6268c7433fbbf57980a309390894c3f25bcd0323
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-09 08:41:56 +0100

    update

--- functions/NumberCountdownGame.R
+++ functions/NumberCountdownGame.R
@@ -50,80 +50,26 @@ while (ran_3digit_num != res) {
   
 }
 
-verbose
-library(dplyr)
+## Part 2
+number_pool <- c(1:11, 25, 50, 75, 100, 200)
+six <- sample(number_pool, 6, replace=FALSE)
+res_num <- sample(100:999,1)
 
-solve_inner <- function(nums, value, expr = value, target, verbose = FALSE) {
-  
-  if (value == target) {
-    if (verbose) {
-      cat(expr, "\n")
+oper <- c("+","-","/","*")
+para <- c("(",")")
+expr <- ""
+
+fun_itsel <- function(six, res_num, expr){
+  for (op in oper){
+    for (ind in seq_along(six)){
+      
+      number <- six[ind]  #paste0('(',six_nm[ind],op,six_nm[ind],')', collapse = NULL)
+      print(res_num)
+      expr <- paste0("(", number, ' ', op, ' ', expr, ")", sep = '')
+      print(expr)
+      
     }
-    return(expr)
-  } else if (length(nums) == 0L) {
-    return()
   }
-  
-  all_res <- c()
-  
-  for (op in c('+', '-', '*', '/')) {
-    for(num_idx in seq_along(nums)) {
-      
-      new_value <- switch(
-        op,
-        '+' = nums[num_idx] + value,
-        '-' = nums[num_idx] - value,
-        '*' = nums[num_idx] * value,
-        '/' = ifelse(nums[num_idx] %% value == 0L, nums[num_idx] %/% value, NA_integer_)
-      )
-      
-      if (!is.na(new_value)) {
-        res <- solve_inner(
-          nums    = nums[-num_idx], 
-          value   = new_value, 
-          expr    = paste("(", nums[num_idx], ' ', op, ' ', expr, ")", sep = ''),
-          target  = target,
-          verbose = verbose
-        )
-        if (!is.null(res)) { all_res <- c(all_res, res) }
-      }    
-      
-      if (op %in% c('-', '/')) {
-        new_value <- switch(
-          op,
-          '-' = value - nums[num_idx],
-          '/' = ifelse(value && nums[num_idx] == 0L, value %/% nums[num_idx], NA_integer_)
-        )
-        
-        if (!is.na(new_value)) {
-          res <- solve_inner(
-            nums    = nums[-num_idx], 
-            value   = new_value, 
-            expr    = paste("(", expr, ' ', op, ' ', nums[num_idx], ")", sep = ''),
-            target  = target,
-            verbose = verbose
-          )
-          if (!is.null(res)) { all_res <- c(all_res, res) }
-        }    
-      }
-    } # num_idx loop
-  }   # op loop
-  
-  all_res 
 }
 
-
-
-solve_countdown <- function(nums, target, verbose = FALSE) {
-  nums   <- as.integer(nums)
-  target <- as.integer(target)
-  seq_along(nums) %>% 
-    purrr::map(~solve_inner(nums[-.x], value = nums[.x], target = target, verbose = verbose)) %>%
-    purrr::flatten_chr() %>%
-    unique()
-}
-
-
-nums   <- c(50, 9, 4, 5, 9, 3)
-target <- 952 
-solve_countdown(nums, target)
+fun_itsel(six,res_num,"")
