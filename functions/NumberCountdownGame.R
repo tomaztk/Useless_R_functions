258b3388edfb0ab32d0208810eca090fbc1236a3
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-10 00:25:33 +0100

    adding new function

--- functions/NumberCountdownGame.R
+++ functions/NumberCountdownGame.R
@@ -17,29 +17,41 @@
 #install.packages("RcppAlgos")
 library(RcppAlgos)
 
-
 number_pool <- c(1:11, 25, 50, 75, 100, 200)
 six <- sample(number_pool, 6, replace=FALSE)
 res_num <- sample(100:999,1)
-oper <- c("+","-","/","*")
 
-
-d2 <- permuteGeneral(six)
-
-for (i in 1:ceiling(length(d2)/6)){
-  for (o in 1:4){
-    r <- paste0(as.integer(d2[i,1]),' ',as.character(sample(oper,1)),' ',as.integer(d2[i,2]),' ',as.character(sample(oper,1)),' ', 
-                as.integer(d2[i,3]),' ',as.character(sample(oper,1)),' ',as.integer(d2[i,4]),' ',as.character(sample(oper,1)),' ', 
-                as.integer(d2[i,5]),' ',as.character(sample(oper,1)),' ',as.integer(d2[i,6]), sep ="")
-    
-    res <- eval(parse(text=r))
-    if(res == res_num){
-      print("solution")
-      print(res)
-      print(r)
-      
+countDown_puzzle <- function(six, res_num) {
+    oper <- c("+","-","/","*")
+    res <- 0
+    d2 <- permuteGeneral(six)
+    for (i in 1:ceiling(length(d2)/6)){
+      for (o in 1:1000){
+        
+        r <- paste0(as.integer(d2[i,1]),' ',as.character(sample(oper,1)),' (',as.integer(d2[i,2]),' ',as.character(sample(oper,1)),' ((', 
+                    as.integer(d2[i,3]),' ',as.character(sample(oper,1)),' ',as.integer(d2[i,4]),') ',as.character(sample(oper,1)),' ', 
+                    as.integer(d2[i,5]),') ',as.character(sample(oper,1)),' ',as.integer(d2[i,6]), ') ', sep ="")
+        #print(r)
+        res <- eval(parse(text=r))
+        if(res == res_num){
+          print(paste0("Solution: ", r, ' with result of: ', res, ' for given vector: ', paste(six, collapse = " "), sep=""));
+       
+            }
+            
+        
+      }
+      rm(i,o,r,res)
     }
-  }
-}
+    }
 
+
+#
+# run function
+#
+
+#countDown_puzzle(c(9,8,50,2,11,200), 352)
+#countDown_puzzle(c(11,50,75,8,3,25), 544)
+#countDown_puzzle(c(25,5,11,7,8,2), 768)
+
+countDown_puzzle(six, res_num)
 
