8ae7c9dd4d3cdce4f9b8261f5906b067b020f1b1
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-01-10 00:40:26 +0100

    adding new function

--- functions/NumberCountdownGame.R
+++ functions/NumberCountdownGame.R
@@ -17,9 +17,6 @@
 #install.packages("RcppAlgos")
 library(RcppAlgos)
 
-number_pool <- c(1:11, 25, 50, 75, 100, 200)
-six <- sample(number_pool, 6, replace=FALSE)
-res_num <- sample(100:999,1)
 
 countDown_puzzle <- function(six, res_num) {
     oper <- c("+","-","/","*")
@@ -29,20 +26,17 @@ countDown_puzzle <- function(six, res_num) {
       for (o in 1:1000){
         
         r <- paste0(as.integer(d2[i,1]),' ',as.character(sample(oper,1)),' (',as.integer(d2[i,2]),' ',as.character(sample(oper,1)),' ((', 
-                    as.integer(d2[i,3]),' ',as.character(sample(oper,1)),' ',as.integer(d2[i,4]),') ',as.character(sample(oper,1)),' ', 
-                    as.integer(d2[i,5]),') ',as.character(sample(oper,1)),' ',as.integer(d2[i,6]), ') ', sep ="")
+                    as.integer(d2[i,3]),' ',as.character(sample(oper,1)),as.integer(d2[i,4]),') ',as.character(sample(oper,1)), 
+                    as.integer(d2[i,5]),') ',as.character(sample(oper,1)),as.integer(d2[i,6]), ') ', sep ="")
         #print(r)
         res <- eval(parse(text=r))
         if(res == res_num){
-          print(paste0("Solution: ", r, ' with result of: ', res, ' for given vector: ', paste(six, collapse = " "), sep=""));
+          print(paste0("Solution: ", r, ' with result of: ', res, ' for given the numbers: ', paste(six, collapse = " "), sep=""));
        
             }
-            
-        
       }
-      rm(i,o,r,res)
     }
-    }
+}
 
 
 #
@@ -52,6 +46,13 @@ countDown_puzzle <- function(six, res_num) {
 #countDown_puzzle(c(9,8,50,2,11,200), 352)
 #countDown_puzzle(c(11,50,75,8,3,25), 544)
 #countDown_puzzle(c(25,5,11,7,8,2), 768)
+countDown_puzzle(c(100,9,10,4,1,8), 594)
+
+
+# or generate the numbers and random solution
+number_pool <- c(1:11, 25, 50, 75, 100, 200)
+six <- sample(number_pool, 6, replace=FALSE)
+res_num <- sample(100:999,1)
 
 countDown_puzzle(six, res_num)
-
+  
