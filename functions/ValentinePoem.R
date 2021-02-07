8449b9932c199875f564248226a6e8d79d366860
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-02-07 07:57:29 +0100

    update

--- functions/ValentinePoem.R
+++ functions/ValentinePoem.R
@@ -5,30 +5,31 @@
 #
 # Series:
 # Little Useless-useful R functions #18
-# Created: February 06, 2021
+# Created: February 07, 2021
 # Author: Tomaz Kastrun
 # Blog: tomaztsql.wordpress.com
 # V.1.0
 #
 # Changelog: 
-#
+#    - sin + sin = heart
+#    - cos + cos = angel wings
 ###########################################
 
 library(tidyverse)
 
 
 ValentinePoem <- function(){
-df<- data_frame(t = seq(-pi, 0, 0.001),
-           x1 = (sin(t)*sin(t)),
+df<- data_frame(sq = seq(-30, 0, 0.005),
+           x1 = (sin(sq)*sin(sq)),
            x2 = x1*-1,
-           y = sqrt(cos(t))*cos(200*t) + sqrt(abs(t)) - 0.7*(4 - t^2)^0.01
+           y = sqrt(cos(sq))*cos(200*sq) + sqrt(abs(sq)) - 0.7*(4 - sq^2)^0.01
            ) %>%
 gather(heart, x,x1,x2)
 p <- ggplot(df, aes(x, y)) + geom_polygon(fill = "Red") + theme_void() +
   geom_text(size=6,aes(x=0, y=0, label="Errors are red, \n
       Reserved words are blue, \n
-      I am here to write a useless\n
-      heart function for you!"), col="black") +  theme(legend.position = "none")
+      I am here writing this useless\n
+      R heart function for you!"), col="black") +  theme(legend.position = "none")
 return(p)
 }
 
