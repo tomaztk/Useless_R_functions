e675a540295bdeaeaf4e3c23eae07cb328443ebf
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-02-15 21:42:09 +0100

    update function

--- functions/L-Systems.R
+++ functions/L-Systems.R
@@ -17,6 +17,7 @@
 library(TurtleGraphics)
 
 
+# common function
 turtlebump <- function(i, j) {
   if (i==0) {
     turtle_forward(10)
@@ -42,7 +43,6 @@ turtle_do({
     turtle_left(90)
     turtle_forward(120)
     turtle_forward(120)
-  
     turtle_right(60)
     turtle_right(60)
     turtle_right(60)
@@ -50,4 +50,46 @@ turtle_do({
     turtlebump(i,j)
   })
 
+###########################################
+###########################################
+### Randomised L-System
+###########################################
+###########################################
 
+random_turtle <- function(){
+    
+    f <- ""
+    single_com <- function(){
+      list_com <- c("turtle_left(","turtle_right(")
+      angle <- sample(1:120, 1, TRUE)
+      com <- sample(list_com,1,TRUE)
+      return(paste0(com, angle, ")\n"))
+    }
+    
+    comm1 <- "set.seed(2908)
+    turtle_init(600, 500, 'clip')
+    turtle_hide()
+    i <- 8
+    j <- 500
+    turtle_do({"
+    
+    for (i in 1:10){
+      sc <- single_com()
+      #sc2 <- single_com2()
+      i <- i + 1
+      f <- paste(f, sc, collapse = NULL)
+      #print(f)
+      comm2 <<- f
+    }
+    
+    comm3 <- "
+    turtlebump(i,j)
+    })"
+    
+    fin <- paste0(comm1, comm2, comm3)
+    eval(parse(text=fin))
+
+}
+
+#run random function
+random_turtle()
