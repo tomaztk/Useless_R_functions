49515d7d15bd854be60bf34e2678b0b716192a2a
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-02-21 21:40:27 +0100

    update L-sys

--- functions/L-systems2.R
+++ functions/L-systems2.R
@@ -1,10 +1,22 @@
 
 # 4 Transformations i.e. p(n+1) = A * p(n) + a
 
-A <- matrix(c( 0   ,  0   ,  0   , 0.16), nrow=2); a = c(0, 0.00); pa = 0.01
-B <- matrix(c( 0.65, -0.10,  0.04, 0.80), nrow=2); b = c(0, 1.60); pb = 0.85
-C <- matrix(c( 0.20,  0.10, -0.26, 0.22), nrow=2); c = c(0, 1.60); pc = 0.07
-D <- matrix(c(-0.25,  0.26,  0.28, 0.24), nrow=2); d = c(0, 0.44); pd = 0.07
+A <-   matrix(c( 0   ,  0   ,  0   , 0.16), nrow=2); 
+a = c(0, 0.00); 
+pa = 0.01
+
+B <- matrix(c( 0.65, -0.10,  0.04, 0.80), nrow=2); 
+b = c(0, 1.60); 
+pb = 0.85
+
+C <- matrix(c( 0.20,  0.10, -0.26, 0.22), nrow=2); 
+c = c(0, 1.60); 
+pc = 0.07
+
+D <- matrix(c(-0.25,  0.26,  0.28, 0.24), nrow=2); 
+d = c(0, 0.44); 
+pd = 0.07
+
 K <- list(A, B, C, D)
 k <- list(a, b, c, d)
 
@@ -14,10 +26,13 @@ s <- sample(1:4, N, prob = c(pa, pb, pc, pd), replace = TRUE)
 
 # Allocate space for the iterated points
 P <- matrix(0, nrow=2, ncol=N+1)
+
 for (i in seq(N)) {
+
   M = K[[s[i]]]
   m = k[[s[i]]]
-  P[,i+1] = M %*% P[,i] + m
+
+    P[,i+1] = M %*% P[,i] + m
 }
 
 # Plot Matrix
