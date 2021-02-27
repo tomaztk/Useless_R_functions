1ed876d0cf80eada6207e48b57f1c8a2cfe144b8
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-02-27 20:47:41 +0100

    adding new file

---
+++ functions/FunctionNames.R
@@ -0,0 +1,39 @@
+# combine
+
+a <- c(0,0,0,0.2)
+
+c <- function(a,b){
+  return("aaaa")
+}
+
+a <- c("a","b")
+#error
+a <- c(0,0,0,0.2)
+
+# True / False
+
+False <- FALSE
+True <- TRUE
+
+isTRUE(True)
+identical(True, TRUE)
+
+
+# sum function
+
+sum <- function(x){
+  mean(x)
+}
+
+sum(c(10,20,40,20))
+
+
+
+#Letters
+
+LETTERS
+letters
+
+
+
+
