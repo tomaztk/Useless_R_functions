f227fb859b08ad985c9caf012e51f6adff08e69e
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-03-07 20:58:02 +0100

    update

--- functions/EncryptText.R
+++ functions/EncryptText.R
@@ -25,3 +25,10 @@ plaintext.serialized <- serialize(plaintext, NULL)
 #Decrypth
 ciphertext <- data_encrypt(plaintext.serialized, key = passkey)
 unserialize(data_decrypt(ciphertext, key = sha256(charToRaw("geslo123"))))
+
+
+### Store passkey as variable
+unpkk <- "136 242  93 138  72 225  66  50 253 250  82 192  58 189   8  47 118  53 141  62  33  28  41 119 227 195 172  85 213  77 241  61"
+
+ciphertext <- data_encrypt(serialize(plaintext, NULL), key = unpkk)
+unserialize(data_decrypt(ciphertext, key = sha256(charToRaw("geslo123"))))
