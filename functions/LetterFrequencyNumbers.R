9073b2e50218256dfa8f38a110abf3b631b4c6ac
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-03-20 07:52:53 +0100

    update

--- functions/LetterFrequencyNumbers.R
+++ functions/LetterFrequencyNumbers.R
@@ -25,29 +25,51 @@ word_a_number <- function(numb){
   
           if (numb <= 20) {
             im <- df[which(df$num == numb),]$let
-            print(paste(im, collapse = NULL))
+            #print(paste(im, collapse = NULL))
           } else {
                   if (numb %% 10 == 0){
                     e <- df[which(df$num == numb),]$let
-                    print(paste0(e, collapse=NULL))
+                    #print(paste0(e, collapse=NULL))
                   } else {
                     sec <- numb %% 10
                     fir <- as.integer(numb/10)*10
                     f_im <- df[which(df$num == fir),]$let
                     s_im <- df[which(df$num == sec),]$let
                     res <- paste0(f_im,"-",s_im, collapse = NULL)
-                    print(res)
+                    #print(res)
                   }
           }
 }
 
-# run function
+# run a single function
 word_a_number(87)
 
-# Automate the function
+
+#function for  count the frequency 
+freqLet <- function(x) {
+  word <- tolower(unlist(strsplit(x,"")))
+  word_table <- table(word)
+  ans <- word_table[names(word_table)]
+}
+
+getFreq <- function(vect) {
+  df <- data.frame(word=as.character(), stringsAsFactors = FALSE)
+  for (i in 1:length(vect)) {
+    df[i,1] <- as.character(word_a_number(i))
+    a <<- freqLet(df$word)
+  }
+  return(a)
+}
+
+######### Let's check the complete set of numbers
+
+# Automate the function, get a vector of first 100 numbers
 vect <- c(1:100)
 
-for (i in 1:length(vect)) {
-  #print(i)
-  word_a_number(i)
-}
+#Is there A in first 100 words?
+getFreq(vect)
+
+#quick visual
+plot(sort(a, decreasing = TRUE), type = "h", col = "red", lwd = 10,
+     main = "Letter frequency in numbers", xlab = "Letters", ylab = "frequnecy of a letter")
+
