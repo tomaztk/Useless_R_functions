d994eed24f87eeef49a3864b4e34b0a98eed3027
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-04-08 17:41:29 +0200

    updated function

--- functions/Useless_API.R
+++ functions/Useless_API.R
@@ -20,6 +20,16 @@ path <- "Users/tomazkastrun/Documents/GitHub/Useless_R_functions/functions"
 
 library(plumber)
 
+
+
 plumb("Documents/GitHub/Useless_R_functions/functions/UselessFun_API.R") %>%
   pr_run(port=2908)
 
+### Get
+# Url1: http://127.0.0.1:2908/sporocilce?msg1=aaaa&msg2=bbbb
+# Url2: http://127.0.0.1:2908/diagram
+# Url3: http://127.0.0.1:2908/produkt?a=2&b=3
+
+
+### Post
+#Url4: http://127.0.0.1:2908/prod?a=4&b=4
