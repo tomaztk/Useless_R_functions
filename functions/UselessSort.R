d6fefc4a864778ae287fb498e56cf7b17442c6ad
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-04-20 17:21:30 +0200

    update

--- functions/UselessSort.R
+++ functions/UselessSort.R
@@ -34,19 +34,21 @@ quacksort <- function(setN){
 #series
 quacksort(series)
 
-### Intosort 
+### Insert sort
 Intosort <- function(A){
-    for (j in 2:length(A)) {
-       key = A[j] +     i = j - 1
-        while (i > 0 && A[i] > key) {
-            A[(i + 1)] = A[i] +       i = i - 1
+     for (j in 2:length(A)) {
+         key = A[j] 
+         i = j - 1
+         while (i > 0 && A[i] > key) {
+             A[(i + 1)] = A[i] 
+             i = i - 1
            }
-         A[(i + 1)] = key
+         key <- A[(i + 1)]
        }
      A
- }
+   }
 
-
-# testing series
 Intosort(series)
+
+
 
