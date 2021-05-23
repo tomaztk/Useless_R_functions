b5e442434ef7b3a067867b56e7e83871f34b3692
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-05-23 05:45:26 +0200

    update

--- functions/Variables_loop_plot.R
+++ functions/Variables_loop_plot.R
@@ -1,25 +1,39 @@
-# Looping through variable names and plotting graph
+##########################################
+# 
+# Looping through variable names and
+# plotting boxplots
+#
+# Series:
+# Little Useless-useful R functions #22
+# Created: May 22, 2021
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+###########################################
+
 
 library(ggplot2)
 
-
-#get columns
+#get column names from sample dataset Iris
 variableR <- names(iris)[1:4]
 
 
-
-Iris_plot <- function(df=iris, .y_var) {
-    y_var <- sym(.y_var)
-  ggplot(df, aes(x = Species, y = !! y_var)) + 
+# Helper function
+Iris_plot <- function(df=iris, y) {
+  ggplot(df, aes(x = Species, y = !! sym(y) )) + 
     geom_boxplot(notch = TRUE) +
-    theme_classic(base_size = 12)
+    theme_classic(base_size = 10)
 }
 
 
+# Main loop through the columns and dataset
 for(varR in variableR){
-  # pdf(paste0(varR, ".pdf"))
-  print(Iris_plot(df=iris,.y_var=varR))
-  # dev.off()
+  name <- paste0(varR, "_x_Species")
+  png(paste0(name, ".png"))
+  print(Iris_plot(df=iris, y=varR))
+  dev.off()
 }
 
 
