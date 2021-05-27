a56bd9b1d4b636a35283c057e9d751488493fa78
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-05-27 05:03:46 +0200

    update

--- functions/IsDatasetSame.R
+++ functions/IsDatasetSame.R
@@ -13,23 +13,12 @@
 # Changelog: 
 ###########################################
 
+
 # two samples of Dataframe
 ds1 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,7))
-ds2 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,7))
-
-#using WriteBin
-zzfil1 <- tempfile("testbin1")
-zz1 <- file(zzfil1, "wb")
-
-zzfil2 <- tempfile("testbin2")
-zz2 <- file(zzfil2, "wb")
-
-d1 <- writeBin(toString(ds1), zz1)
-d2 <- writeBin(toString(ds2), zz2)
-
-#check
-identical(d1,d2)
+ds2 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,8),col3=c(4,5,6,7))
 
 
 #using equality with all.equal
 all.equal(ds1,ds2,check.attributes = TRUE, use.names = TRUE)
+
