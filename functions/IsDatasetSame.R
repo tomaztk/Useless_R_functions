19f4cad9e1c027645441fc4f54cb60d5a680c2d0
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-05-27 05:14:21 +0200

    update

--- functions/IsDatasetSame.R
+++ functions/IsDatasetSame.R
@@ -12,13 +12,20 @@
 
 # Changelog: 
 ###########################################
-
+library("compareDF")
+#install.packages("compareDF")
 
 # two samples of Dataframe
 ds1 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,7))
-ds2 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,8),col3=c(4,5,6,7))
+ds2 <- data.frame(col1=c("t1","t2","t3","s4"),col2=c(1,2,5,8))
 
 
 #using equality with all.equal
 all.equal(ds1,ds2,check.attributes = TRUE, use.names = TRUE)
+
+
+# Using compareDF difference / equality
+
+df_compare <- compare_df(ds1, ds2, "col2")
+df_compare$comparison_df
 
