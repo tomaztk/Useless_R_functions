f25e3b2d28dc4b96236d4aa2288c251db6486117
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-06-20 09:09:29 +0200

    adding fun

---
+++ functions/GeneratingCookingRecipes.R
@@ -0,0 +1,20 @@
+##########################################
+# 
+# Generating cooking recipes
+#
+# Series:
+# Little Useless-useful R functions #24
+# Created: June 20, 2021
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+# 
+# Changelog: 
+###########################################
+
+ingredients <- c()
+quantites <- c()
+actions <- c()
+
+
+
