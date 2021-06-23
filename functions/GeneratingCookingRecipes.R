b02f72d521373c8b22424244febd2284ae0c404d
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-06-23 16:35:24 +0200

    update

--- functions/GeneratingCookingRecipes.R
+++ functions/GeneratingCookingRecipes.R
@@ -17,6 +17,7 @@ quantites <- c("Piece(s)", "Gram(s)", "Liter(s)", "Bowl")
 actions <- c("Slice","Bake","Refrigerate","Cook","Steam","Dip","Leave to Rest", "grill")
 Kitchen_supply <- c("spatula", "oven", "refrigerator", "pan", "sauce-pan", "whisk")
 steps <- c("1-1", "1-2-1", "1-2-2", "2", "1")
+steps_des <- c("Take ", "and")
 
-
+sample(ingredients, 1, replace = F)
 
