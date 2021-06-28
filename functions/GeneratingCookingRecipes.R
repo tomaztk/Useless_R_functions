2bbb6c3d0b9140f83c8cd2f36cf2a3dbad635347
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-06-28 21:38:04 +0200

    update

--- functions/GeneratingCookingRecipes.R
+++ functions/GeneratingCookingRecipes.R
@@ -25,3 +25,22 @@ string_2 <- as.vector(sample(actions,1,replace=F))
 
 recipe <- paste(string_1,string_2,sep = " ")
 
+
+
+RandomRecipe <- function(s=0, numbers=0, symbols=0, lowerCase=0, upperCase=0) {
+  ASCII <- NULL
+  symbols <- 0
+  if(symbols>0)    ASCII <- c(ASCII, sample(c(33:47, 58:34, 91:96, 123:126), symbols))
+  if(numbers>0)    ASCII <- c(ASCII, sample(48:57, numbers))
+  if(upperCase>0)  ASCII <- c(ASCII, sample(65:90, upperCase))
+  if(lowerCase>0)  ASCII <- c(ASCII, sample(97:122, lowerCase))
+  if(characters>0) ASCII <- c(ASCII, sample(c(65:90, 97:122), characters))
+  
+  cat(rawToChar(as.raw(sample(ASCII, length(ASCII)))) )
+}
+
+RandomRecipe(10)
+
+
+
+
