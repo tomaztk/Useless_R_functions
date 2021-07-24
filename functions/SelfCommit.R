8a9ec5795cd31a77d34f23f980cf341e9ca75ff7
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-07-24 22:02:03 +0200

    update

--- functions/SelfCommit.R
+++ functions/SelfCommit.R
@@ -11,12 +11,11 @@ PlotIris <- function() {
 ggplot(data=iris, aes(x = Sepal.Length, y = Sepal.Width)) +
   geom_point(aes(color=Species, shape=Species))
 
-system("cd /Users/tomazkastrun/Documents/GitHub/Useless_R_functions/functions && git add SelfCommit.R && git commit -m 'update' && git push", intern = TRUE)
+system("sudo cd /Users/tomazkastrun/Documents/GitHub/Useless_R_functions/functions && git add SelfCommit.R && git commit -m 'update' && git push", intern = TRUE)
 #system("git add SelfCommit.R", intern = TRUE)
 #system("git commit -m 'update' ")
 #system("git push")
 
 }
-
 
 PlotIris()
