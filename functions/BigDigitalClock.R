f0b9ca5c4ba443d1078650c6c8d97896484f638a
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-09-15 20:56:56 +0200

    another update

--- functions/BigDigitalClock.R
+++ functions/BigDigitalClock.R
@@ -123,8 +123,18 @@ rm(df0, df1,df2,df3,df4,df5,df6,df7,df8,df9, dfc, dfs,n0,n1,n2,n3,n4,n5,n6,n7,n8
 
 #print(numbers)
 
-getVariable <- function(x = integer()){
-  stopifnot(is.integer(x))
+getVariable <- function(x) {
+  stopifnot(is.numeric(x))
+  if (x == 0) {return (numbers$n0)}
+  if (x == 1) {return (numbers$n1)}
+  if (x == 2) {return (numbers$n2)}
+  if (x == 3) {return (numbers$n3)}
+  if (x == 4) {return (numbers$n4)}
+  if (x == 5) {return (numbers$n5)}
+  if (x == 6) {return (numbers$n6)}
+  if (x == 7) {return (numbers$n7)}
+  if (x == 8) {return (numbers$n8)}
+  if (x == 9) {return (numbers$n9)}
   
 }
 
@@ -134,8 +144,6 @@ BigDitigalClock <- function() {
   while(TRUE){
     Sys.sleep(1)
     cat("\014")
-    print.data.frame(numbers,  row.names = F)
-    colnames(numbers) <- c(" "," "," "," "," "," "," "," "," "," "," "," ")
     
     #hour
     hh <- strftime(Sys.time(), format="%H")
@@ -152,14 +160,18 @@ BigDitigalClock <- function() {
     s1 <- substr(ss,1,1)
     s2 <- substr(ss,2,2)
     
+    current_time <- cbind(getVariable(as.integer(h1)), getVariable(as.integer(h2)), numbers$colon, 
+                          getVariable(as.integer(m1)), getVariable(as.integer(m2)), numbers$colon,
+                          getVariable(s1), getVariable(s2))
     
-    
-    #cat("\r", strftime(Sys.time(), format="%H:%M:%S"))
+    colnames(current_time) <- c(" "," "," "," "," "," "," "," ")
+    print.data.frame(current_time,  row.names = F)
   }
 }
 
 
-#BigDitigalClock()
+BigDitigalClock()
+
 
 
 
