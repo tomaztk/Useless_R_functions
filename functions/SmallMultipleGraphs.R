0b002d82278276806668c9b1878b2841f4c59e1f
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-15 20:33:12 +0200

    update

--- functions/SmallMultipleGraphs.R
+++ functions/SmallMultipleGraphs.R
@@ -12,7 +12,7 @@
 # Changelog: 
 ###########################################
 
-set.seed(2900)
+set.seed(2908)
 
 # 1. Making data of 20 cases
 myData <- data.frame(value=rnorm(2000, mean = 40, sd=20))
@@ -20,8 +20,8 @@ myControls <- data.frame(value=rnorm(2000, mean = 30, sd=20))
 cases <- NULL
 controls <- NULL
 for (i in 1:20) {
-  cases[[i]] <- sample(myData$value, size=32)
-  controls[[i]] <- sample(myControls$value, size=32)
+  cases[[i]] <- sample(myData$value, size=20)
+  controls[[i]] <- sample(myControls$value, size=20)
 }
 
 estimates <- data.frame(lower=NA, 
@@ -60,27 +60,26 @@ fakeData<-data.frame(value=rnorm(1000000, mean=10, sd=popDifferenceSE))
 # 2. Get some libs for plotting
 
 # get libraries we need for plotting and stacking the plots
-library(grid)
-library(gridExtra)
 library(ggplot2)
 
 
-problemColors <- c("TRUE"="red", "FALSE"="darkgrey")
+problemColors <- c("TRUE"="red", "FALSE"="blue")
 colorScale <- scale_colour_manual(name="problem", values=problemColors)
 
 
-smallMultiples <- ggplot(data=estimates, aes(x=meanDiff, y=sampleNum)) +
+# Plot Graph
+ggplot(data=estimates, aes(x=meanDiff, y=sampleNum)) +
   
   geom_errorbarh(aes(xmin=lower,xmax=upper, color=problem)) +
   geom_point(aes(color=problem))  + 
-  geom_vline(xintercept = 10, color="blue") +
+  geom_vline(xintercept = 10, color="darkgreen") +
   scale_y_reverse() + 
   geom_text(aes(x=-24, y=sampleNum, 
                 label=paste("Mean:", round(meanDiff,2))),
             size = 2.5, hjust="inward") +
   
   geom_text(aes(x=-17, y=sampleNum, 
-                label=paste("p: ",
+                label=paste("P(x): ",
                             p, sep="")),
             size = 2.5, hjust="inward") +
   
@@ -98,13 +97,10 @@ smallMultiples <- ggplot(data=estimates, aes(x=meanDiff, y=sampleNum)) +
             size = 2.5, hjust="inward") 
 
 
-# 3. Final plotting
-finalGraph <- grid.arrange(smallMultiples,ncol = 1, heights = c(10, 1))
+
 
 
 # 4. Clean
 rm(popDifferenceSE,i,se,tBound, zBound, cases, controls, myData, myControls, tTest)
 rm(colorScale, estimates, fakeData, smallMultiples, finalGraph, problemColors)
-
-
 
