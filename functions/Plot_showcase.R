c322eda287675b0bc1331453bbb95a7b989619dc
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-17 19:34:22 +0200

    update

--- functions/Plot_showcase.R
+++ functions/Plot_showcase.R
@@ -37,7 +37,7 @@ plot_animation <- function(SavePath){
                               ,"plot(BarData, main = 'Histogram')"
                               ,"plot(BarData, rnorm(150), main = 'Boxplot')"
                               ,"plot(TimeSeriesData, main = 'Time-series')"
-                              ,"plot(fun, -10, 5*pi, main = 'Plot a function')"
+                              ,"plot(FunctionData, -10, 5*pi, main = 'Plot a function')"
                               ,"plot(IrisData, main = 'Correlation plot for two variables')"
                               ,"plot(IrisData, main = 'Correlation plot for two variables with lines of SS')
                                  lines(lowess(iris[,1:2]))"
@@ -51,7 +51,7 @@ plot_animation <- function(SavePath){
                                    ,'TimeSeriesData <- ts(matrix(rnorm(300),  
                                         nrow = 300, ncol = 1), start = c(1990, 1), 
                                         frequency = 12)'
-                                   ,'fun <- function(x) {x^4*pi}'
+                                   ,'FunctionData <- function(x) {x^4*pi}'
                                    ,'IrisData <- as.data.frame(iris[, 1:2])'
                                    ,'IrisData <- as.data.frame(iris[, 1:2])'
                                    ,'IrisData3 <- as.data.frame(iris[, 1:3])'
@@ -69,10 +69,10 @@ plot_animation <- function(SavePath){
           p <- eval(parse(text=graphinfo))
           
           plot(c(0, 1), c(0, 1), ann = FALSE, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-          text(x = 0.5, y = 0.5, paste(graphinfo), cex = 1.5, col = "Darkgreen", font=1, adj=0.5)
+          text(x = 0.5, y = 0.5, paste("Graph code: ", graphinfo), cex = 1.5, col = "Darkgreen", font=1, adj=0.5)
           
           plot(c(0, 1), c(0, 1), ann = FALSE, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-          text(x = 0.5, y = 0.5, paste(datainfo), cex = 1.5, col = "Darkblue", font=1, adj=0.5)
+          text(x = 0.5, y = 0.5, paste("Data script: ",datainfo), cex = 1.5, col = "Darkblue", font=1, adj=0.5)
           
           par(mfrow=c(1,1)) 
           dev.off()
