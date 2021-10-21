5a3d9d1e1cca75ffc743383f5ebe60a73b472bfe
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-21 15:19:51 +0200

    adding sample

---
+++ functions/LearningIrregularVerbs.R
@@ -0,0 +1,23 @@
+##########################################
+# 
+# Irregular verbs - English
+#
+# Series:
+# Little Useless-useful R functions #30
+# Created: October 21, 2021
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+#
+###########################################
+
+
+df <- data.frame(verb=c("drink","drive")
+        ,past=c("drank","drove")
+        ,pastp=c("drunk","driven")
+)
+
+
+
+df
+
