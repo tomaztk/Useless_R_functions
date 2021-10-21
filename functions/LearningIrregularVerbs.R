ef6b028bbbeacf0b4c424048dc7799db78703e73
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-21 21:44:22 +0200

    update of irregular verbs

--- functions/LearningIrregularVerbs.R
+++ functions/LearningIrregularVerbs.R
@@ -11,13 +11,47 @@
 #
 ###########################################
 
+learnVerbs <- function() {
 
-df <- data.frame(verb=c("drink","drive")
-        ,past=c("drank","drove")
-        ,pastp=c("drunk","driven")
-)
+        # populate the list of irregular verbs
+        df <- data.frame(verb=c("drink","drive","cut","arise","fight","flee","become","begin","spin")
+                         ,past=c("drank","drove","cut","arose","fought","fled","became","began","spun")
+                         ,past_perfect=c("drunk","driven","cut","arisen","fought","fled","become","begun","spun")
+        )
+        exit <- "ex"
+        correct <- 0
+        wrong <- 0
+
+
+        repeat {
+          cat("\014") 
+          randomWord <- df[sample(nrow(df), 1), ]
+          randomWord[1]
+          formW <- sample(c("past","past_perfect"), 1)
+          promptText <- paste0("Find the __", formW, "__ form for the verb >>", toupper(randomWord[1,1]),"<< : ")
+          promptText
+          inputW <- readline(prompt=promptText)
+          if (inputW == randomWord[1,formW]) {
+            print("bravo")
+            correct <- correct + 1
+            barplot(table(c(replicate(correct, "correct"), replicate(wrong, "wrong"))))
+            randomWord <- ""
+        
+          } else {
+            print("naaah")
+            wrong <- wrong +  1
+            barplot(table(c(replicate(correct, "correct"), replicate(wrong, "wrong"))))
+            randomWord <- ""
+          }
+          if (inputW == exit){
+            wrong <- wrong -  1
+            break
+            barplot(table(c(replicate(correct, "correct"), replicate(wrong, "wrong"))))
+          }
+        }
+
+}
 
 
 
-df
-
+learnVerbs()
