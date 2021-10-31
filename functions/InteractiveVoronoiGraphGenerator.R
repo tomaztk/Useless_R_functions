df4bd5c47cc80c4ad4196cf8f88a160d24ae85b4
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-31 06:45:07 +0100

    update

--- functions/InteractiveVoronoiGraphGenerator.R
+++ functions/InteractiveVoronoiGraphGenerator.R
@@ -12,19 +12,22 @@
 # packages
 library(deldir)
 library(ggplot2)
-dff <- NULL
+dff <- data.frame(NULL,NULL)
 n <- 0
 
 
 #### Board Graph 
 voronoiGraphBoard <- function(){
- r <<-  ggplot(data=dff, aes(x=long,y=lat)) +
+if (nrow(dff)<= 2) {
+  plot.new()
+} else{
+r <<-  ggplot(data=dff, aes(x=long,y=lat)) +
     geom_segment( aes(x = x1, y = y1, xend = x2, yend = y2), size = 2, data = voronoi$dirsgs, linetype = 1, color= "#FFB958") + 
     geom_point( fill=rgb(70,130,180,255,maxColorValue=255), pch=21, size = 4,color="#333333") 
   
  r
 }
-
+}
 
 
 
@@ -35,25 +38,21 @@ click <- function(voronoiGraphBoard = defaultGraph){
       long <- rnorm(1, mouse.at$x*10,15)
       lat <- rnorm(1, mouse.at$y*10,10)
       df <- data.frame(lat,long)
-      dff <- rbind(dff, df)
+      dff <<- rbind(dff, df)
       
       if (nrow(dff)>=2){
         voronoi <- deldir(dff$long, dff$lat)
-        voronoiGraphBoard()
-        
+        defaultGraph <<- voronoiGraphBoard()
       }
       n <- n + 1
-    }  
-      Sys.sleep(1)
-      defaultGraph <<- voronoiGraphBoard
+       
   }
-  
-
+}
 
 
 ### Start with x11 
 Draw_x11 <- function(){
-  x11()
+  #x11()
   defaultGraph <<- voronoiGraphBoard() 
   click()
 }
