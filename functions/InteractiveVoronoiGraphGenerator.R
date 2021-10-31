6f9ea6562f29cdb51b63a0749328365c711e9949
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-10-31 07:15:56 +0100

    update

--- functions/InteractiveVoronoiGraphGenerator.R
+++ functions/InteractiveVoronoiGraphGenerator.R
@@ -1,17 +1,24 @@
-#######################
+##########################################
+# 
+# Interactive Voronoi graph generator 
+#  with R
 #
-# Interactive Voronoi
-# graph generator
-#
-#
-#
-#######################
+# Series:
+# Little Useless-useful R functions #30
+# Created: November 01, 2021
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
 
+# Changelog: 
+#        - adding x11() / quartz
+###########################################
 
 
 # packages
 library(deldir)
 library(ggplot2)
+
 dff <- data.frame(NULL,NULL)
 n <- 0
 
@@ -20,16 +27,14 @@ n <- 0
 voronoiGraphBoard <- function(){
 if (nrow(dff)<= 2) {
   plot.new()
-} else{
-r <<-  ggplot(data=dff, aes(x=long,y=lat)) +
+} else {
+   ggplot(data=dff, aes(x=long,y=lat)) +
     geom_segment( aes(x = x1, y = y1, xend = x2, yend = y2), size = 2, data = voronoi$dirsgs, linetype = 1, color= "#FFB958") + 
     geom_point( fill=rgb(70,130,180,255,maxColorValue=255), pch=21, size = 4,color="#333333") 
   
- r
+ 
+ }
 }
-}
-
-
 
 click <- function(voronoiGraphBoard = defaultGraph){
     for (n in 1:10) {
@@ -39,22 +44,22 @@ click <- function(voronoiGraphBoard = defaultGraph){
       lat <- rnorm(1, mouse.at$y*10,10)
       df <- data.frame(lat,long)
       dff <<- rbind(dff, df)
-      
+      print(n)
       if (nrow(dff)>=2){
         voronoi <- deldir(dff$long, dff$lat)
         defaultGraph <<- voronoiGraphBoard()
       }
-      n <- n + 1
-       
   }
 }
+
 
 
 ### Start with x11 
 Draw_x11 <- function(){
   #x11()
-  defaultGraph <<- voronoiGraphBoard() 
+  #defaultGraph <<- voronoiGraphBoard() 
   click()
+  defaultGraph
 }
 
 # Generate Voronoi
