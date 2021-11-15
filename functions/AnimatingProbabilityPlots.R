6578405947b9f80e2bacea5b9a0c031deb7d26a2
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-11-15 05:40:58 +0100

    adding file distributions

---
+++ functions/AnimatingProbabilityPlots.R
@@ -0,0 +1,17 @@
+##########################################
+# 
+# Probability Plots
+#
+# Series:
+# Little Useless-useful R functions #28
+# Created: November 11, 2021
+# Author: Tomaž Kaštrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+###########################################
+
+distributions <- c("dbeta", "dbinom", "dcauchy", "dchisq", "dexp", "dgamma",  "dgeom",  "dhyper", 
+                   "dlnorm", "dmultinom", "dnbinom", "dnorm", "dpois", "dt", "dunif", "dweibull", "wilcox", "logis", "hyper", "tukey")
+
+
