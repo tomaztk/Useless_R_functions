310e915c66f54724854ffb87c138b76fc87e7c9c
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-11-21 19:57:02 +0100

    update table creation

--- functions/TSQL_2048.sql
+++ functions/TSQL_2048.sql
@@ -2,10 +2,15 @@ USE QL;
 
 
 DECLARE @dim INT = 4
-DECLARE @i INT  = 1
 
-DECLARE @TableCreate VARCHAR(2000) = 
-'DROP TABLE IF EXISTS dbo.T_2048
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- CREATE TABLE for @dim dimension
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DECLARE @i INT = 1
+DECLARE @j INT = 1
+
+DECLARE @TableCreate NVARCHAR(2000) = 
+'DROP TABLE IF EXISTS dbo.T_2048; 
 CREATE TABLE dbo.T_2048 ('
 
 WHILE (@dim >= @i)
@@ -14,16 +19,16 @@ BEGIN
 	SET @TableCreate = @TableCreate + 'V' + CAST(@i AS VARCHAR(10)) + ' SMALLINT ,'
 	SET @i = @i + 1
 END
-SET @TableCreate = STUFF(@TableCreate, LEN(@TableCreate), 1, ')')
+SET @TableCreate = STUFF(@TableCreate, LEN(@TableCreate), 1, ');')
 
-SET @TableCreate = '
-INSERT INTO dbo.T_2048 VALUES ('
+WHILE (@dim >= @j)
+BEGIN
+	SET @TableCreate = @TableCreate + ' 
+	INSERT INTO dbo.T_2048 VALUES ('
+	+ STUFF(REPLICATE('0,',@dim), LEN(REPLICATE('0,',@dim)), 1, ');') 
+	SET @j = @j+1
+END
 
-GO 4
-
-
-EXEC sp_sqlexec @tableCreate
-
-
+EXEC sp_executesql @tableCreate
 
 SELECT * FROM T_2048
