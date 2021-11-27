18cbfedba0a5251bceca2c67a7052a247a7cfd1b
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-11-27 18:20:28 +0100

    update

--- functions/TSQL_2048.sql
+++ functions/TSQL_2048.sql
@@ -113,38 +113,24 @@ AS
 BEGIN
 
 	IF @move = 'U'
-	SELECT 'U'
+		BEGIN
+			EXEC dbo.MOVE_up @dim
+			SELECT * FROM T_2048
+		END
+
 	IF @move = 'D'
-	SELECT 'D'
+		BEGIN
+			EXEC dbo.MOVE_down @dim
+			SELECT * FROM T_2048
+		END
+
+
 	IF @move = 'L'
 	SELECT 'L'
 	IF @move = 'R'
 	SELECT 'R'
 END;
 GO
-
-DECLARE @ii INT = 0
-DECLARE @dim INT = 4
-
-WHILE @dim > @ii
-BEGIN
-	SELECT 
-		CASE 
-			WHEN p2.V2 > p1.v2 AND P1.V2 = 0 THEN  p2.V2
-			WHEN  p2.V2 = P1.V2  THEN p2.v2 + p1.v2 END as NewVal
-			,@ii as position
-			,NULL as PrevVal
-	FROM T_2048 as P1
-	joIN T_2048 as P2
-	ON p1.ID = p2.id+1
-	WHERE p1.id = @dim-@ii
-
-	
-	EXEC dbo.UPDATE_position 2,3,2
-	EXEC dbo.UPDATE_position 2,3,2
-
-	SET @ii = @ii + 1
-END
 
 
 -- ===========================================
@@ -161,11 +147,9 @@ EXEC dbo.INIT_matrix 4  --( == EXEC dbo.ADD_number ) -- Add new number 2 on rand
 SELECT * FROM T_2048
 
 -- Play
-EXEC dbo.MAKE_move 'R'
+EXEC dbo.MAKE_move 'D', 4
 
 
-
--- SELECT * FROM T_2048
 
 
 
@@ -430,7 +414,7 @@ end
 select * from tt
 
 
----- UP complete
+
 
 ---- --------------------------
 ---- --------------------------
@@ -446,7 +430,7 @@ BEGIN
 
 
 	DECLARE @Column_counter INT = 2
-	Declare @max_column INT = (SELECT 4 /* dim */ + 2)
+	Declare @max_column INT = (SELECT @dim /* dim */ + 2)
 
 
 	while @max_column > @Column_counter
@@ -470,17 +454,16 @@ BEGIN
 		---------------------------------------
 					-- UP scenarij
 						DECLARE @ii int = 1
-						while 4-1 >= @ii -- število dimenzij
+						while @dim-1 >= @ii -- število dimenzij
 						BEGIN
 							declare @i int = 1
 
-							while 4 > @i -- 
+							while @dim > @i -- 
 							--declare @i int = 1 
 							begin	
 								declare @vv_1 int = (select v from #temp where id = @i)
 								declare @vv_2 int = (select v from #temp where id = @i+1)
-								print @vv_1
-								print @vv_2
+
 							IF (@vv_1 = 0 AND @vv_2 <> 0)
 							BEGIN
 								update #temp set v = @vv_2 where id = @i   
@@ -528,7 +511,95 @@ BEGIN
 END
 
 
+---- --------------------------
+---- --------------------------
+---- MOVE DOWN Procedure
+---- --------------------------
+---- --------------------------
 
 
+CREATE OR ALTER PROCEDURE dbo.MOVE_down
+		@dim INT
+AS
+BEGIN
+
+	DECLARE @Column_counter INT = 2
+	Declare @max_column INT = (SELECT @dim /* dim */ + 2)
 
 
+	while @max_column > @Column_counter
+	BEGIN
+		-- Get first column name
+		DECLARE @col_name VARCHAR(10) = (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'T_2048' and ORDINAL_POSITION = @Column_counter)
+		print @col_name
+
+		DECLARE @sql1 NVARCHAR(2000) =
+		'SELECT  id, ' + @col_name + ' as v FROM T_2048'
+
+		DROP TABLE IF EXISTS #temp;
+		create table  #temp (id int, v int)
+	
+		insert into #temp
+		EXEC sp_executesql @sql1
+
+		--SELECT * FROM #temp
+		/* premik */
+
+		---------------------------------------
+					-- DOWN scenarij
+
+						DECLARE @ii int = 1
+						while @dim-1 >= @ii -- število dimenzij
+						BEGIN
+							declare @i int = @dim
+
+							while 1 < @i  
+							begin	
+								declare @vv_1 int = (select v from #temp where id = @i)
+								declare @vv_2 int = (select v from #temp where id = @i-1)
+
+							IF (@vv_1 = 0 AND @vv_2 <> 0)
+							BEGIN
+								update #temp set v = @vv_2 where id = @i   
+								update #temp set v = 0     where id = @i-1 
+							END
+
+							IF (@vv_1 <> 0 AND @vv_1 = @vv_2)
+							BEGIN
+								update #temp set v = @vv_1 + @vv_2 where id = @i
+								update #temp set v = 0 where id = @i-1
+							END
+
+							IF (@vv_1 <> 0 AND @vv_2  = 0) 
+							BEGIN
+								Print 'Do nothing'
+							END
+
+							set @i = @i - 1
+							end
+						  set @ii = @ii + 1
+						END
+
+				-- update back to T_2048 table from #temp table
+
+				DECLARE @sql_temp_update NVARCHAR(500)
+
+				SET @SQL_temp_update = 
+				'UPDATE T20
+					SET '+@col_name+' = t.v
+			
+				FROM t_2048  AS T20
+				JOIN #temp AS t
+				ON T20.id = t.id'
+			
+				EXEC sp_executesql @SQL_temp_update
+
+		----------------------------------------
+
+		SET @Column_counter = @Column_counter + 1
+
+		-- END; Show T_2048
+	
+
+	   END
+END
