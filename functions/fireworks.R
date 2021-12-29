3da95fa22533ebba01d285c50e82372046e53733
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-12-29 21:29:36 +0100

    nifty update

--- functions/fireworks.R
+++ functions/fireworks.R
@@ -19,29 +19,40 @@ library(animation)
 library(ggplot2)
 
 
-
-set.seed(99)
-x <-sample(1:50,10)
-y <-sample(1:500,10)
+set.seed(2908)
 
 
-#  l.col = c("rainbow", "red", "green")
-
-par(bg = 'black')
-plot(x, y, xaxt='n', ann=FALSE, yaxt='n', frame.plot=FALSE, xlim=c(0,50),ylim=c(0,500))
-
-symbols(x=c(33,44,22), y=c(484,415,68), circles=c(0.16, 0.4, 0.25),add=T, inches=F, fg=c('red', 'blue', 'green'))
-
-symbols(x = 33, y = 484, circles=c(0.22),add=T, inches=F, fg="blue")
-
-
-fireworks <- function(){
+Fireworks <- function(n = 10) {
+  if (!interactive()) return()
+  x <-sample(1:50,10)
+  y <-sample(1:500,10)
+  color <- rainbow(n)
   
-  ani.options(interval = 1)
-  for (i in length(x)){
+  bgcolor <- par("bg")
+  if (bgcolor == "transparent") bgcolor <- "black"
+  
+    draw.fireworks <- function() {
+      for (i in 1:15) {   # število rink
+          plot(x, y, xaxt='n', ann=FALSE, yaxt='n', frame.plot=FALSE, xlim=c(0,50),ylim=c(0,500))
+          symbols(x=c(33,44,22), y=c(484,415,68), circles=c(0.16, 0.4, 0.25),add=T, inches=F, fg=c("red", "green"))
+          symbols(x = 33, y = 484, circles=c(0.22),add=T, inches=F, fg="blue")        
+      }
+    }
     
-  }
-  
-  ani.pause()
-  
+    clear.fireworks <- function(){
+      a <- 2
+      #no idea what to do here :)
+    }
+
+    ani.options(interval = 1)
+    
+    for (i in length(x)){
+      draw.fireworks()
+      clear.fireworks()
+    }
+    ani.pause()
 }
+
+
+
+Fireworks(5)
