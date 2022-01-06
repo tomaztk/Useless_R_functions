70ba204728ebd7a480ba17cd320b7b5a8cf573a6
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-01-06 15:47:14 +0100

    update

--- functions/MastermindGame.R
+++ functions/MastermindGame.R
@@ -233,158 +233,156 @@ numberOfPegs <- 4
 numberOfColors <- 4
 numberOfTries <- 10
 
+get_board <- function(nof_col, nof_try=10){
+  plot.new()
+  op <- par(bg = "white")
+  grid(nx = 6, ny = 12, col = "gray", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  
+  # adding boarders 
+  par(new = TRUE)
+  plot(c(100,500), c(100,500), xlab = "", ylab = "", xaxt = 'n', yaxt = 'n',main = "Mastermind board game")
+  nof_tries <- nof_try
+  for (i in 1:nof_tries) { #rows
+    for (j in 1:nof_col) { #columns
+      col <- 50*(1:nof_col-1)
+      # 6 per row | every second row empyt | start top - down
+      rect(100+col[j], 500-(i*30), 150+col[j], 475-(i*30), col = 'white')
+    }
+  }
+  colours = c('Red','Green','Blue','Yellow','Brown','Orange')
+  for (z in 1:nof_col) {
+    rect(100+z*50, 100, 150+z*50, 150, col = colours[z])
+  } 
+}
+
+
+add_rect <- function(colour,try,nof_try=10) {
+  par(new = TRUE)
+  max_tries <- numberOfColors*nof_try #10 rows
+  if (try %% numberOfColors == 0) { #number of tries = number of colours
+    nof_try <- try/numberOfColors
+    add_key_pegs(input_colours,store_secret, nof_try)
+  } 
+  
+  if (try > numberOfColors){
+    row <- ceiling((try/numberOfColors))
+    rect_order <- abs(try-((row-1)*numberOfColors))
+  } else {
+    row <- 1
+    rect_order <- try
+  }
+  rect(100+(rect_order*50)-50, 500-(row*30),150+((rect_order*50))-50, 475-(row*30), col = colour)
+}
+
+
+add_key_pegs <- function(input_colours, store_secret,nof_try){
+  
+  ss <- store_secret
+  ic <- input_colours
+  
+  black <- length(which(ss==ic))
+  white <- abs(length(which(ic %in% ss)==TRUE) - black) 
+  #nof_try <- 1
+  nof_tokes <- black + white
+  tok <- replicate(black, "black")
+  en <- replicate(white, "white")
+  token <- c(tok, en)
+  name1 <- token[1]
+  name2 <- token[2]
+  name3 <- token[3]
+  name4 <- token[4]
+  
+  if (nof_tokes == 1) {
+    par(new = TRUE)
+    plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+  }
+  if (nof_tokes == 2) {
+    par(new = TRUE)
+    plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+  }
+  if (nof_tokes == 3) {
+    par(new = TRUE)
+    plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(470,500-(nof_try*30), col = name3, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+  }
+  if (nof_tokes == 4) {
+    par(new = TRUE)
+    plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(470,500-(nof_try*30), col = name3, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+    par(new = TRUE)
+    plot(470,490-(nof_try*30), col = name4, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
+  }
+}
+
+get_secret <- function(nof_col, colours_repeat=FALSE) {
+  colours <- c(1:4)
+  s <- sample(colours,nof_col, replace=colours_repeat) 
+  s <- paste(s, collapse="")
+  return(as.integer(s))
+}
+
 
 game <- function(numberOfColors=4, numberOfTries=10){
-  won <- FALSE
-  end_game <- 1
-  
-  get_secret <- function(nof_col, colours_repeat=FALSE) {
-    colours <- c('Red','Green','Blue','Yellow') #,'Brown','Orange')
-    sample(colours,nof_col, replace=colours_repeat)
-  }
-  
-  
-  get_board <- function(nof_col, nof_try=10){
-    plot.new()
-    op <- par(bg = "white")
-    grid(nx = 6, ny = 12, col = "gray", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+   end_game <- 1
+   
+   get_board(nof_col = numberOfColors, nof_try = numberOfTries)
+   store_secret <<- get_secret(nof_col=numberOfColors, colours_repeat =TRUE)
+   input_colours <<- 0L
+   #input_colours <- c("Blue", "Yellow","Green","Yellow") #, "Brown", "Green")
+   nof_selection <- numberOfColors
+   max_tries <- nof_selection*numberOfTries 
+ 
+   while (end_game <= max_tries && store_secret != input_colours) {
+     print(end_game)
+     mouse.at <- locator(n = 1, type = "o") 
+     x.at <<- mouse.at$x
+     y.at <<- mouse.at$y
+     
+     
+     if (x.at >= 150 & x.at < 200 & y.at >= 100 & y.at <=150) {
+       input_colours <<- as.integer(paste( c(input_colours, 1),collapse=""))
+       add_rect('Red',end_game) 
+     }
+     if (x.at >= 200 & x.at < 250 & y.at >= 100 & y.at <=150) {
+       input_colours <<- as.integer(paste( c(input_colours, 2),collapse=""))
+       add_rect('Green',end_game) 
+     }
+     if (x.at >= 250 & x.at < 300 & y.at >= 100 & y.at <=150) {
+       input_colours <<- as.integer(paste( c(input_colours, 3),collapse=""))
+       add_rect('Blue',end_game) 
+     }
+     if (x.at >= 300 & x.at < 350 & y.at >= 100 & y.at <=150) {
+       input_colours <<- as.integer(paste( c(input_colours, 4),collapse=""))
+       add_rect('Yellow',end_game) 
+     } 
     
-    # adding boarders 
-    par(new = TRUE)
-    plot(c(100,500), c(100,500), xlab = "", ylab = "", xaxt = 'n', yaxt = 'n',main = "Mastermind board game")
-    nof_tries <- nof_try
-    for (i in 1:nof_tries) { #rows
-      for (j in 1:nof_col) { #columns
-        col <- 50*(1:nof_col-1)
-        # 6 per row | every second row empyt | start top - down
-        rect(100+col[j], 500-(i*30), 150+col[j], 475-(i*30), col = 'white')
-      }
-    }
-    colours = c('Red','Green','Blue','Yellow','Brown','Orange')
-    for (z in 1:nof_col) {
-      rect(100+z*50, 100, 150+z*50, 150, col = colours[z])
-    } 
-  }
-  
-  add_rect <- function(colour,try,nof_try=10) {
-    par(new = TRUE)
-    max_tries <- numberOfColors*nof_try #10 rows
-    if (try %% numberOfColors == 0) { #number of tries = number of colours
-      nof_try <- try/numberOfColors
-      add_key_pegs(input_colours,store_secret, nof_try)
-    } 
-    
-    if (try > numberOfColors){
-      row <- ceiling((try/numberOfColors))
-      rect_order <- abs(try-((row-1)*numberOfColors))
-    } else {
-      row <- 1
-      rect_order <- try
-    }
-    rect(100+(rect_order*50)-50, 500-(row*30),150+((rect_order*50))-50, 475-(row*30), col = colour)
-  }
-  
-  
-  add_key_pegs <- function(input_colours, store_secret,nof_try){
-    
-    ss <- store_secret
-    ic <- input_colours
-    
-    black <- length(which(ss==ic))
-    white <- abs(length(which(ic %in% ss)==TRUE) - black) 
-    #nof_try <- 1
-    nof_tokes <- black + white
-    tok <- replicate(black, "black")
-    en <- replicate(white, "white")
-    token <- c(tok, en)
-    name1 <- token[1]
-    name2 <- token[2]
-    name3 <- token[3]
-    name4 <- token[4]
-    
-    if (nof_tokes == 1) {
-      par(new = TRUE)
-      plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-    }
-    if (nof_tokes == 2) {
-      par(new = TRUE)
-      plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-    }
-    if (nof_tokes == 3) {
-      par(new = TRUE)
-      plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(470,500-(nof_try*30), col = name3, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-    }
-    if (nof_tokes == 4) {
-      par(new = TRUE)
-      plot(450,500-(nof_try*30), col = name1, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(450,490-(nof_try*30), col = name2, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(470,500-(nof_try*30), col = name3, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-      par(new = TRUE)
-      plot(470,490-(nof_try*30), col = name4, lwd = 2, xaxt = 'n', yaxt = 'n', xlab = "",ylab = "",xlim=range(100:500), ylim=range(100:500))
-    }
-    
-    
-  }
-  
-  # store secret
-  store_secret <- get_secret(nof_col=numberOfColors, colours_repeat =TRUE)
-  #input_colours <- character(length = numberOfColors)
-  input_colours <- NULL
-  #input_colours <- c("Blue", "Yellow","Green","Yellow") #, "Brown", "Green")
-  get_board(nof_col = numberOfColors, nof_try = numberOfTries)
-  
-  nof_selection <- numberOfColors
-  max_tries <- nof_selection*numberOfTries 
-  
-  while (end_game <= max_tries & won == FALSE) {
-    for (i in 1:max_tries) {
-      if (end_game %% numberOfColors != 0) {
+          #clean vector
+         if (end_game %% numberOfColors == 0)  { input_colours <<- 0L }
         
-        mouse.at <- locator(n = 1, type = "o") 
-        x.at <<- mouse.at$x
-        y.at <<- mouse.at$y
-        if (x.at >= 150 & x.at < 200 & y.at >= 100 & y.at <=150) {
-          input_colours <- c(input_colours, 'Red')
-          add_rect('Red',i) 
-        }
-        if (x.at >= 200 & x.at < 250 & y.at >= 100 & y.at <=150) {
-          input_colours <- c(input_colours, 'Green')
-          add_rect('Green',i) 
-        }
-        if (x.at >= 250 & x.at < 300 & y.at >= 100 & y.at <=150) {
-          input_colours <- c(input_colours, 'Blue')
-          add_rect('Blue',i) 
-        }
-        if (x.at >= 300 & x.at < 350 & y.at >= 100 & y.at <=150) {
-          input_colours <- c(input_colours, 'Yellow')
-          add_rect('Yellow',i) 
-        }
-      } else {
-        add_key_pegs(input_colours=input_colours, store_secret=store_secret,nof_try=10){
-          
-          if (store_secret == input_colours) {
-            print("Game Won")
-            break
-          }
-          input_colours <- character(length = numberOfColors)
-        }
-      }
-      end_game <- end_game + 1
-      
-    }
-    graphics.off()
-  }
+         #end game if needed 
+         if (store_secret == input_colours) {
+           par(new = TRUE)
+           mtext("GAME WON", side=1)
+           break
+         }
+
+     
+     # increment next level
+     end_game = end_game + 1
+     
+   }
+    
+}
   
   
-  
-  #Start The game
-  game()
+#Start The game
+game()
   
