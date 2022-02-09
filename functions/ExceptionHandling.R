886e407e94e9af02a15457618ea57bdb08cf6b4f
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-02-09 06:31:50 +0100

    update

--- functions/ExceptionHandling.R
+++ functions/ExceptionHandling.R
@@ -18,24 +18,46 @@ test_stop(22,10)
 test_stop(10,22)
 
 
-input_list <- list(1, 2, 4, -5, 'oh no', 10, 0)
+
+#warning(), message() and supressWarnings
+
+test_warning <- function(min, max){
+    if(max < min) {
+      warning("Warning, warning. Max < Min")
+    }
+    if(max > min) {
+      message("Well, max > min")
+    }
+}
+
+test_warning(10,20)
+test_warning(20,10)
+
+suppressWarnings(test_warning(20,10))
+suppressWarnings(test_warning(10,12))
+
+
+
+# example of error!
+
+# using list, instead of vector of character
+# so output control is better
+input_list <- list(1, 2, 3, 'R Rules', 10, -500, 1.23, 0, 1024)
 
 for(inp in input_list) {
-     print(paste("Power of 2 of", inp, "=", inp**2))
+   print(paste("Power to the 2 of", inp, "is: ", inp**2))
 }
 
 
 # Try
-input_list_try <- list(1, 2, 4, -5, 'oh no', 10, 0)
-
 # with silent ON
-for(inp in input_list_try) {
-  try(print(paste("Power of 2 of", inp, "=", inp**2)), silent = TRUE)
+for(inp in input_list) {
+  try(print(paste("Power to the 2 of", inp, "is: ", inp**2)), silent = TRUE)
 }
 
 # without silent
-for(inp in input_list_try) {
-  try(print(paste("Power of 2 of", inp, "=", inp**2)))
+for(inp in input_list) {
+  try(print(paste("Power to the 2 of", inp, "is: ", inp**2)))
 }
   
 
