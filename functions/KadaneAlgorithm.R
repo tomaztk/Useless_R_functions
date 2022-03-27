fa9c9dad072b20d621440dba72aa56bbff819ab9
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-03-27 22:24:33 +0200

    Update kadane algorithm

--- functions/KadaneAlgorithm.R
+++ functions/KadaneAlgorithm.R
@@ -14,23 +14,66 @@
 ###########################################
 
 
-install.packages('adagio')
+## Home edition
+v <- c(-3,-8, 1, -2, 1, 5, -3, -4, 3, 10, -2, 4, 1)
+
+
+kadane <- function(v){
+  
+  max_so_far = -999999999999999 #some obnoxiously big number
+  max_ending_here = 0
+  start = 0
+  end = 0
+  s = 0
+  
+  for (i in 1:length(v)) {
+    max_ending_here = max_ending_here  + v[i]
+    if (max_so_far < max_ending_here ){
+      max_so_far = max_ending_here
+      start = s
+      end = i
+    }
+    if (max_ending_here < 0) {
+      max_ending_here = 0
+      s = i+1
+    }
+  }
+  #return (max_so_far)
+  cat("Sum is: ", max_so_far, " with starting position: ", start, " and ending: ", end)
+}
+
+
+kadane(v)
+
+
+
+#########################
+#using adagio R package!
+########################
+
+#install.packages('adagio')
 library(adagio)
 
 
 # single vector
-maxsub(c(-2, -3, 4, -1, -2, 1, 5, -3), inds = TRUE)
-  
+maxsub(v,  inds = TRUE)
 
-##  Standard example: Find a maximal sum submatrix
-A <- matrix(c(0,-2,-7,0, 9,2,-6,2, -4,1,-4,1, -1,8,0,2),nrow = 4, ncol = 4, byrow =TRUE)
+
+
+#  Standard example: Find a maximal sum submatrix
+A <- matrix(c(3,-2,-7,4, 9,2,-6,1, -10,2,-4,1, -5,7,2,-2),nrow = 4, ncol = 4, byrow =TRUE)
+
 maxsub2d(A)
+
 
 
 #  Application to points in the unit square:
 set.seed(723)
-N <- 50; w <- rnorm(N)
-x <- runif(N); y <- runif(N)
+
+N <- 50; 
+w <- rnorm(N)
+x <- runif(N); 
+y <- runif(N)
 clr <- ifelse (w >= 0, "blue", "red")
 plot(x, y, pch = 20, col = clr, xlim = c(0, 1), ylim = c(0, 1))
 
