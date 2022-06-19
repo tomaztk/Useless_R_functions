3887bec39a342e60e3ba0a48e223741a10e41d40
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-06-19 22:47:06 +0200

    update

--- functions/binary_octal_decimal_conversion.R
+++ functions/binary_octal_decimal_conversion.R
@@ -1,11 +1,20 @@
-##################
-#
-# Conversion to-from
-# Binary, octal, Decimal
-#
-##################
+##########################################
+# 
+# Plotting decimal to binary conversion
+# 
+# Series:
+# Little Useless-useful R functions #39
+# Created: June 20, 2022
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+#        
+###########################################
 
 
+## Function to convert binary to decimal
 bin2dec <- function(bin){
   
   bin_ss <- as.numeric(strsplit(as.character(bin),"")[[1]])
@@ -19,6 +28,7 @@ return(paste("Binary", bin, "is ",res," in decimal"))
 }
 
 
+## Function to convert decimal to binary
 dec2bin <- function(dec){
   dec_start <- dec
   str <- ''
@@ -33,14 +43,38 @@ dec2bin <- function(dec){
   splits <- strsplit(str, "")[[1]]
   reversed <- rev(splits)
   f_str <- paste(reversed, collapse = "")
-  return(paste("Decimal", dec_start, "is ",f_str," in binary"))
+  #return(paste("Decimal", dec_start, "is ",f_str," in binary"))
+  return(f_str)
 }
 
 
 # Test
-dec <- 236
-bin <- 11101100
+bin2dec(11101100)
+dec2bin(236)
 
-bin2dec(bin)
-dec2bin(dec)
+
+
+
+
+### Draw scatter plot for the conversion
+df <- data.frame(dec_x = 1, bin_y = 1, digit_length=1)
+
+for (i in 2:100){
+    d <- c(dec_x = i, bin_Y = dec2bin(i), nchar(dec2bin(i)))
+   df <-  rbind(df,d)
+}
+
+# change formats
+df$dec_x <- as.numeric(df$dec_x)
+df$bin_y <- as.numeric(df$bin_y)
+
+
+library(ggplot2)
+library(cowplot)
+
+line <- ggplot(df, aes(x=dec_x, y=bin_y, colour=digit_length)) + geom_line()
+line2 <- ggplot(df, aes(x=dec_x, y=bin_y)) + geom_line() + geom_smooth()
+
+plot_grid(line, line2, labels = c("Length of binary digits", "Like a binominal distribution"))
+
 
