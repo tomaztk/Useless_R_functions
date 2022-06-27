fb618c99f5fe335f5a2888411a6d6adc1a5e4daf
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-06-27 22:51:24 +0200

    useless update

--- functions/ShortenURL.R
+++ functions/ShortenURL.R
@@ -41,3 +41,34 @@ ShortenURL("https://medium.com/@tomazkastrun/culture-fit-or-culture-add-e89ca048
 
 
 # Create QR Code
+
+library(qrcode)
+library(tidyverse)
+library(ggplot2)
+
+text <- "https://medium.com/@tomazkastrun/culture-fit-or-culture-add-e89ca0485ed1"
+color <- "green"
+x <- qrcode_gen(text, plotQRcode=F, dataOutput=T)
+x <- as.data.frame(x)
+
+y = x
+y$id <- rownames(y)
+
+y <- gather(y, "key", "value", colnames(y)[-ncol(y)])
+
+# y$key = factor(y$key, levels=rev(colnames(x)))
+# y$id = factor(y$id, levels=rev(rownames(x)))
+
+ggplot(y, aes(x=id, y=key)) + geom_tile(aes(fill=value), alpha=alpha)
+
+## Test 2
+x <- qr_code(text, ecl="M")
+x <- as.data.frame(x)
+
+y = x
+y$id <- rownames(y)
+
+y <- gather(y, "key", "value", colnames(y)[-ncol(y)])
+
+ggplot(y, aes(x=id, y=key)) + geom_tile(aes(fill=value), alpha=alpha) # + scale_fill_gradient(low=White, high = color) + theme_void() 
+                                           
