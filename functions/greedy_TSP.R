b1bc88717b5e2a6609486ad6d46b8d6a1c04be3a
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-08-14 18:24:31 +0200

    update

--- functions/greedy_TSP.R
+++ functions/greedy_TSP.R
@@ -1,21 +1,37 @@
 
-#############################
-### Greedy algorithm for TSP (Traveling salesman problem)
-#############################
+##########################################
+# 
+# Greedy TSP algorithm 
+# (TSP = Traveling Salesman Problem)
+#
+# Series:
+# Little Useless-useful R functions #41
+# Created: August 13, 2022
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+
+# Changelog: 
+#        
+###########################################
+
+
 library(ggplot2)
 library(tidyverse)
 
-#data
+
+
+#generate fake data
+df_size <- 100
+
 set.seed(2908)
-cities <- data.frame(x = sample(rnorm(1,100,20),1000,replace=TRUE),
-                     y = sample(rnorm(1,100,15),1000,replace=TRUE),
-                     unvisited = 0,
-                     rn = 1:1000)
-
-# plot(cities$x~cities$y)
+cities <- data.frame(x = sample(rnorm(1,100,20),df_size,replace=TRUE),
+                     y = sample(rnorm(1,100,15),df_size,replace=TRUE),
+                     rn = 1:df_size)
 
 
-# helper function
+
+# Nearest neighbor function
 nearest_neighbour <- function(pos, cities){
   id <- which.min(colSums((t(cities) - pos)^2))
   idx <- cities[id, ]
@@ -23,42 +39,41 @@ nearest_neighbour <- function(pos, cities){
 }
 
 
-#algorithm
+# Greedy algorithm
 greedy_algo <- function(cities){
   pos.ix = (cities[sample(1:nrow(cities), 1), ])
+  pos.ix.c <- c(pos.ix$x,pos.ix$y)
   tour <<- c(NULL)
   unvisited <<- cities[!(cities$rn %in% pos.ix$rn), ]
-  while (unvisited$unvisited == 0) {
-    C = nearest_neighbour(pos.ix, unvisited)
+  while (nrow(unvisited) > 0) {
+    C = nearest_neighbour(pos.ix.c, unvisited)
     tour <<- rbind(tour, C)
     
     unvisited <<- unvisited[!(unvisited$rn %in% C$rn), ]
-    pos.ix <- tail(tour)
-    #print(pos.ix)
+    pos.ix <- tail(tour,1)
+    pos.ix.c <- c(pos.ix$x, pos.ix$y)
     
   }
   return(tour)
 }
 
 
-#run greedy
-greedy_algo(cities)
-
-tourQ <- data.frame(x=tour$x,y=tour$y)
-
+# Plot path
 plot_path <- function(df){
   for (i in 1:nrow(df)){
     dt_temp <- head(df,i)
-    p  <- ggplot(dt_temp, aes(x = dt_temp$x, y = dt_temp$y)) +
-      geom_line() +
-      geom_point()
-    plot(p)
-    Sys.sleep(0.2)
-    i <- i + 1
+    p <<- ggplot() + geom_point(data=df, aes(x = x, y = y), colour="black", size = 1) +  geom_point(data=dt_temp, aes(x=x,y=y), colour="red", size = 1)
+    #plot(p)
+    #Sys.sleep(0.01)
   }
-
+  plot(p)
 }
 
-# solution
+
+
+#run greedy
+greedy_algo(cities)
+
+# plot  solution
 plot_path(tour)
 
