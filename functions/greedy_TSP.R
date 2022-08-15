b6acf0e15c76d3e34a0dee03d55652136310ea61
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-08-15 09:25:55 +0200

    adding animation

--- functions/greedy_TSP.R
+++ functions/greedy_TSP.R
@@ -20,61 +20,59 @@ library(ggplot2)
 library(tidyverse)
 
 
-
 #generate fake data
 df_size <- 50
-
 set.seed(2908)
+
 cities <- data.frame(x = sample(rnorm(1,100,10),df_size,replace=TRUE),
-                     y = sample(rnorm(1,100,10),df_size,replace=TRUE)
-                     ,
+                     y = sample(rnorm(1,100,10),df_size,replace=TRUE),
                      rn = 1:df_size)
 
 
-# Abosolute Nearest neighbor function
-nearest_neighbour_absolute <- function(pos, cities){
+#plot(cities$y~cities$x)
+
+
+# Absolute Nearest city function
+nearest_city_absolute <- function(pos, cities){
   if (which.min(colSums((t(cities[,c("x","y")]) - pos)^2)) >= which.min(colSums((t(cities[,c("y","x")]) - pos)^2))) {
     id <- which.min(colSums((t(cities[,c("x","y")]) - pos)^2))
   } else {
     id <- which.min(colSums((t(cities[,c("y","x")]) - pos)^2))
   }
-  
-  idx <- cities[id, ]
-  return(idx)
+  return(cities[id, ])
 }
 
-# Nearest neighbour function for coordinate system
-nearest_neighbour_coordinate <- function(pos, cities){
+# Nearest city function for coordinate system (where x and y must not be switched!)
+nearest_city_coordinate <- function(pos, cities){
   id <- which.min(colSums((t(cities[,c("x","y")]) - pos)^2))
-  idx <- cities[id, ]
-  return(idx)
+  return(cities[id, ])
 }
 
 
-# Greedy algorithm
-greedy_algo <- function(cities){
+# Greedy TSP algorithm
+greedy_TSP <- function(cities){
   pos.ix <- (cities[sample(1:nrow(cities), 1), ])
   pos.ix.c <- c(pos.ix$y,pos.ix$x)
-  tour <- c(NULL)
   tour <- pos.ix
   unvisited <- cities[!(cities$rn %in% pos.ix$rn), ]
   while (nrow(unvisited) > 0) {
     pos.ix <- tail(tour,1)
     pos.ix.c <- c(pos.ix$x, pos.ix$y)
-    C <- nearest_neighbour_coordinate(pos.ix.c, unvisited)
-    tour <- rbind(tour, C)
-    unvisited <- unvisited[!(unvisited$rn %in% C$rn), ]
+    found <- nearest_city_coordinate(pos.ix.c, unvisited)
+    tour <- rbind(tour, found)
+    unvisited <- unvisited[!(unvisited$rn %in% found$rn), ]
 
   }
   tour$name <- as.character(seq.int(nrow(tour)))
   return(tour)
 }
 
+
 # Plot path
 plot_path <- function(df){
   for (i in 1:nrow(df)){
     dt_temp <- head(df,i)
-    p2 <- ggplot() + geom_point(data=df, aes(x = x, y = y), colour="black", size = 1) +  geom_point(data=dt_temp, aes(x=x,y=y), colour="red", size = 1, label=dt_temp$name)
+    #p2 <- ggplot() + geom_point(data=df, aes(x = x, y = y), colour="black", size = 1) +  geom_point(data=dt_temp, aes(x=x,y=y), colour="red", size = 1, label=dt_temp$name)
     #plot(p)     # each step
     #Sys.sleep(0.01)
   }
@@ -82,11 +80,18 @@ plot_path <- function(df){
   plot(p)
 }
 
-#run greedy
-#greedy_algo(cities)
-
-# plot  solution
-#plot_path(tour)
 
 #Run together
-plot_path(greedy_algo(cities))
+plot_path(greedy_TSP(cities))
+
+
+##############
+## Animation
+##############
+library(gganimate)
+ggplot(data=mm, aes(x = x, y = y, label=name) , colour="red", size = 2) +
+  geom_point(show.legend = FALSE) +
+  labs(title = 'Step #n: {as.numeric(format(round(frame_time, 2), nsmall = 2))}', x= 'X-axis', y =  'Y-axis') +
+  transition_time(as.numeric(name)) +
+  guides(alpha = F) +
+  shadow_mark(past = T, future=F, alpha=0.3, colour="red")
