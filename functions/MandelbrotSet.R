eac8b65c24be4be5c88847555c5c598063775932
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-10-27 06:31:47 +0200

    update

--- functions/MandelbrotSet.R
+++ functions/MandelbrotSet.R
@@ -14,34 +14,23 @@
 # Changelog: 
 #
 ##########################################
- 
 
-
-cols <- colorRampPalette(c("blue","yellow","red","black"))(11)
-
-# Parameters
-xmin <- -2
-xmax <- 1
-nx <- 250
-ymin <- -1.5
-ymax <- 1.5
-ny <- 250
+require("grDevices") 
+cols <- colorRampPalette(c("black","white","grey","black"))(11)
 n <- 400
 
 
 # variables
-x <- seq(xmin, xmax, length.out=nx)
-y <- seq(ymin, ymax, length.out=ny)
+x <- seq(-2, 1, length.out=250)
+y <- seq(-1.5, 1.5, length.out=250)
 c <- outer(x,y*1i,FUN="+")
 z <- matrix(0.0, nrow=length(x), ncol=length(y))
 k <- matrix(0.0, nrow=length(x), ncol=length(y))
 
 
-
 for (rep in 1:n) { 
-  #print(rep)
-  for (i in 1:nx) { 
-    for (j in 1:ny) { 
+  for (i in 1:250) { 
+    for (j in 1:250) { 
       if(Mod(z[i,j]) < 2 && k[i,j] < n) {
         z[i,j] <- z[i,j]^2 + c[i,j]
         k[i,j] <- k[i,j] + 1
@@ -50,12 +39,10 @@ for (rep in 1:n) {
   }
 }
 
-image(x,y,k, col=cols)
+image(x,y,k, col=cols, axes = FALSE, main = "The Mandelbrot set", xlab = "" , ylab = "" )
+contour(k, add = TRUE, drawlabels = FALSE)
 
 
-df <- data.frame(x=x,  y=y)
-
-ggplot(df, aes(x=x, y=y,color=cols)) + geom_point()
 
 
 
