909a9b1060514a48772a42ac2fcb81eb460d56a4
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-11-20 11:03:42 +0100

    adding image

--- functions/Generating_image.R
+++ functions/Generating_image.R
@@ -28,3 +28,16 @@ image(outer(1:a,1:b,funk))
 
 
 image(outer(1:10, 1:12, "log"))
+
+image(outer(1:10, 1:12, "*"), useRaster = TRUE)
+
+
+x <- y <- seq(-4*pi, 4*pi, len=27)
+r <- sqrt(outer(x^2, y^2, "+"))
+z <- cos(r^2)*exp(-r/6)
+image <- (z - min(z))/diff(range(z))
+
+image(x,y,z)
+
+z <- matrix(runif(500*500), ncol=500)
+image(z)
