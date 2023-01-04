2f3943f4ca336da750ab6bed1582ba1a2c716137
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-01-04 06:02:52 +0100

    update

--- functions/MandelbrotSet.R
+++ functions/MandelbrotSet.R
@@ -14,26 +14,34 @@
 #
 ##########################################
 
-cols <- colorRampPalette(c("white","black","white","grey","black"))(11)
-n <- 400
 
-# variables
-x <- seq(-2, 1, length.out=250)
-y <- seq(-1.5, 1.5, length.out=250)
-c <- outer(x,y*1i,"+")
-z <- matrix(0.0, nrow=length(x), ncol=length(y))
-k <- matrix(0.0, nrow=length(x), ncol=length(y))
-
-
-for (rep in 1:n) { 
-  for (i in 1:250) { 
-    for (j in 1:250) { 
-      if(Mod(z[i,j]) < 2 && k[i,j] < n) {
-        z[i,j] <- z[i,j]^2 + c[i,j]
-        k[i,j] <- k[i,j] + 1
+MandelBrotImage <- function(){
+    
+    cols <- colorRampPalette(c("white","black","white","grey","black"))(11)
+    n <- 400
+    
+    # variables
+    x <- seq(-2, 1, length.out=250)
+    y <- seq(-1.5, 1.5, length.out=250)
+    c <- outer(x,y*1i,"+")
+    z <- matrix(0.0, nrow=length(x), ncol=length(y))
+    k <- matrix(0.0, nrow=length(x), ncol=length(y))
+    
+    
+    for (rep in 1:n) { 
+      for (i in 1:250) { 
+        for (j in 1:250) { 
+          if(Mod(z[i,j]) < 2 && k[i,j] < n) {
+            z[i,j] <- z[i,j]^2 + c[i,j]
+            k[i,j] <- k[i,j] + 1
+          }
+        }
       }
     }
-  }
+    
+    image(x,y,k, col=cols, axes = FALSE, xlab = "" , ylab = "" )
 }
 
-image(x,y,k, col=cols, axes = FALSE, xlab = "" , ylab = "" )
+
+# run function
+MandelBrotImage()
