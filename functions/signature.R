03cf2dd64bc6c3dc24490c96b872c0c4676b1adb
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-02-21 21:32:38 +0100

    Update signature.R

--- functions/signature.R
+++ functions/signature.R
@@ -7,9 +7,10 @@ inscrp <- function(nof){
           plot(x,y, pch = 1, col = "white",  xaxt='n',  yaxt='n', ann=FALSE, frame.plot=FALSE)
           xspline(x,y, 1, draw = TRUE, col="blue")
 }
-
 ##### run
+par(mfrow = c(2,1))
 
 inscrp(10)
-inscrp(50)
-inscrp(100)
+inscrp(20)
+
+par(mfrow = c(1,1))
