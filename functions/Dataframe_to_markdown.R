08c8cada51d5adc0590530d864f463af03cbd670
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-03-26 10:16:07 +0200

    Update Dataframe_to_markdown.R

--- functions/Dataframe_to_markdown.R
+++ functions/Dataframe_to_markdown.R
@@ -3,49 +3,45 @@
 ## e.g:
 ## 
 
-# > iris[1:2,1:3]
-# Result
-
-# Sepal.Length Sepal.Width Petal.Length
-# 1          5.1         3.5          1.4
-# 2          4.9         3.0          1.4
+# > iris[1:3,1:5]
+# Result:
+#> Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+#> 1          5.1         3.5          1.4         0.2  setosa
+#> 2          4.9         3.0          1.4         0.2  setosa
+#> 3          4.7         3.2          1.3         0.2  setosa
 
 # and I want the result to be a markdown script, that can be used in Markdown document:
 
-# |Sepal.Length |Sepal.Width | Petal.Length |
-#   |---|---|---|
-#   |5.1         |3.5          |1.4|
-#   |4.9         |3.0          |1.4|
+#> |Sepal.Length|Sepal.Width|Petal.Length|Petal.Width|Species|
+#> |---|---|---|---|---|
+#> |5.1|3.5|1.4|0.2|setosa|
+#> |4.9|3|1.4|0.2|setosa|
+#> |4.7|3.2|1.3|0.2|setosa|
 
 
 
-your_df <- iris[1:2,1:3]
-
+your_df <- iris[1:3,1:5]
 
 DF_2_MD <- function(your_df){
-
-   cn <- as.character(names(your_df))
-   
-
-    headr <- paste0(c("", cn),  sep = "|")
-    sepr <- paste0(c(rep(paste0(c("|",paste0(rep("-", 3) , sep = "" ))), length(cn)), "|"))
-    sepr <- sub(' ','_', sepr)
-    st <- "|"
+  
+  cn <- as.character(names(your_df))
+  headr <- paste0(c("", cn),  sep = "|", collapse='')
+  sepr <- paste0(c('|', rep(paste0(c(rep('-',3), "|"), collapse=''),length(cn))), collapse ='')
+  st <- "|"
     for (i in 1:nrow(your_df)){
       for(j in 1:ncol(your_df)){
-        if (i%%ncol(your_df) == 0) {
-          st <- paste0(st, your_df[i,j], "|", "\n" , "|")
+        if (j%%ncol(your_df) == 0) {
+          
+          st <- paste0(st, your_df[i,j], "|", "\n", "" , "|", collapse = '')
         } else {
-        st <- paste0(st, your_df[i,j], "|")
+        st <- paste0(st, your_df[i,j], "|", collapse = '')
         }
       }
     }
-    
-     fin <- paste(c(headr, sepr, st), sep="\n")
-    #fin <- toString(paste0(c(headr, '\n', sepr, '\n', st)), collapse="")
+  fin <- paste0(c(headr, sepr, st), collapse="\n")
   cat(fin)
 }  
 
-
 # run function
 DF_2_MD(your_df)
+
