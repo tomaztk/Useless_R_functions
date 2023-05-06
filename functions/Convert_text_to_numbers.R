d5f2a1c586d498af8eeb05472225d920124a42d3
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-05-06 17:11:38 +0200

    update

--- functions/Convert_text_to_numbers.R
+++ functions/Convert_text_to_numbers.R
@@ -17,11 +17,20 @@
 ##########################################
 
 #helper data
-let <- c("a","b","c","","d","e","f","","g","h","i","","j","k","l","","m","n","o","","p","q","r","s","t","u","v","","w","x","y","z")
-mm <- matrix(let,  nrow = 8, ncol=4, byrow = TRUE, dimnames = list(
-                                                            c("N2","N3","N4","N5","N6","N7","N8","N9"),
-                                                            c("P.1","P.2","P.3","P.4")))
+let <- c("a","b","c","",
+         "d","e","f","",
+         "g","h","i","",
+         "j","k","l","",
+         "m","n","o","",
+         "p","q","r","s",
+         "t","u","v","",
+         "w","x","y","z")
 
+mm <- matrix(let, nrow = 8, ncol=4, byrow = TRUE, 
+             dimnames = list(
+                  c("N2","N3","N4","N5","N6","N7","N8","N9"),
+                  c("P.1","P.2","P.3","P.4"))
+              )
 
 
 SMSconverter <- function(tt){
