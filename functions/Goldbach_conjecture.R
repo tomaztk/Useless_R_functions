5cce12d8896d49a1b6a85873b51a58e8612de892
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-08-12 21:03:26 +0200

    update

--- functions/Goldbach_conjecture.R
+++ functions/Goldbach_conjecture.R
@@ -28,30 +28,76 @@ sieve_of_sundaram <- function(limit) {
       j <- j + 1
     }
   }
-  primes <- c(2, (2 * (1:n)+1)[sieve])
+  primes <- c(2,(2*(1:n)+1)[sieve])
   return(primes)
 }
 
-#limit <- 100
-#prime_numbers <- sieve_of_sundaram(limit)
+
+#list of all primes until "limit"
+#sieve_of_sundaram(limit)
 
 
 
-nth_max <- 100
-kl <- list(
-   k <- 1:as.integer(floor(2.4 * nth_max * log(nth_max) / 2))
-  ,integers_list <- replicate(length(k),FALSE)
-)
-
-pcount <- 0
-for (i in (1:k+1)){
-  if (is.na(integers_list[i]) == TRUE){
-    pcount <- pcount + 1
-    if (pcount %% 10 == 0)  {
-      print(k)
-    }
+# is prime
+is_prime <- function(n) {
+  if (n <= 1)  return(FALSE)
+  if (n <= 3)  return(TRUE)
+  if (n %% 2 == 0 || n %% 3 == 0) return(FALSE)
+  i <- 5
+  while (i*i <= n) {
+    if (n %% i == 0 || n %% (i + 2) == 0) return(FALSE)
+    i <- i + 6
   }
+  return(TRUE)
+}
+  
+
+## goldbach for even numbers
+goldbach_conjecture <- function(even_num) {
+  if (even_num <= 2 || even_num %% 2 != 0) {
+    return("Number must be even and greater than 2.")
+  }
+  c <- NULL
+  for (i in 2:(even_num / 2)) {
+    if (is_prime(i) && is_prime(even_num - i)) {
+      #cat("Goldbach's pairs for", even_num, "are:", i, "+", even_num - i, "\n")
+      c <- cbind(c,i) # nof solutions
+    } 
+  }
+  #return(length(c))
+  return(c)
+}
+
+# test
+goldbach_conjecture(870)
+
+
+#make some 1000 solutions
+sol <- NULL
+for (i in seq(4,1000, by=2)){
+  nof_solutions <- goldbach_conjecture(i)
+  sol <- rbind(sol, data.frame(n=i, nof=nof_solutions))
 }
 
 
+# plot solutions; alternating solutions
+plot(sol$n, sol$nof, type = "p", xlab = "Even number", ylab = "Number of Solutions", main = "Goldbach's Conjecture") 
+reg<-lm(nof ~ n, data = sol)
+abline(reg, col="red")
+
+
+#most frequent primes:
+fre <- NULL
+for (i in seq(4,1000, by=2)){
+  sols <- goldbach_conjecture(i)
+  fre <- cbind(fre, sols)
+}
+
+
+solutions_freq<- data.frame(table(t(fre)))
+
+solutions_freq <- solutions_freq[which(as.integer(solutions_freq$Freq) > 1),]
+plot(x=solutions_freq$Var1, y=solutions_freq$Freq,
+     xlab = "Prime number", ylab = "Frequency of prime in sum", main = "Frequencies of prime numbers for
+     Goldbach's Conjecture for first \n 1000 even integers.")
 
