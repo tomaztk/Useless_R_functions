b2f80462ce2df9e391bcb9278e78cad0fef6a2e2
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-08-12 19:43:54 +0200

    update

--- functions/Goldbach_conjecture.R
+++ functions/Goldbach_conjecture.R
@@ -16,22 +16,35 @@
 #
 ##########################################
 
+# sieve of sundaram
+sieve_of_sundaram <- function(limit) {
+  n <- (limit - 1) %/% 2
+  sieve <- rep(TRUE, n + 1)
+  
+  for (i in 1:n) {
+    j <- 1
+    while (i+j+2*i*j <= n) {
+      sieve[i+j+2*i*j] <- FALSE
+      j <- j + 1
+    }
+  }
+  
+  primes <- c(2, (2 * (1:n)+1)[sieve])
+  
+  return(primes)
+}
+
+#limit <- 100
+#prime_numbers <- sieve_of_sundaram(limit)
+
+
 
 nth_max <- 100
 kl <- list(
    k <- 1:as.integer(floor(2.4 * nth_max * log(nth_max) / 2))
   ,integers_list <- replicate(length(k),FALSE)
 )
-#k <- as.integer(floor(2.4 * nth_max * log(nth_max) / 2))
-#integers_list <- TRUE * k
 
-for (i in 1:k){
-  j <- 1
-  while(i+j+2*i*j < k){
-    j <- j + 1
-    integers_list[i+j+2*i*j] <- FALSE
-  }
-}
 pcount <- 0
 for (i in (1:k+1)){
   if (is.na(integers_list[i]) == TRUE){
@@ -41,3 +54,6 @@ for (i in (1:k+1)){
     }
   }
 }
+
+
+
