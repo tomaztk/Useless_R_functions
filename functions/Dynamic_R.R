103ec9fc2f85718fe6dacafa379dbad9586d89f9
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-08-28 06:22:49 +0200

    adding dynamic R

---
+++ functions/Dynamic_R.R
@@ -0,0 +1,27 @@
+# dynamic R
+
+
+dynamic <- "result <- 2 * 3"
+eval(parse(text = dynamic))
+print(result)
+
+
+base_packages = getOption('defaultPackages')
+names(base_packages) = base_packages
+
+bsp <- lapply(base_packages, function (pkg) ls(paste0('package:', pkg)))
+bs <- ls("package:base")
+
+
+sample(bsp$datasets,1)
+
+
+
+dynamic <- "
+base_packages <- getOption('defaultPackages');
+bsp <- lapply(base_packages, function (pkg) ls(paste0('package:', pkg)));
+ds<-sample(bsp$datasets,1);
+"
+
+eval(parse(text = dynamic))
+head(ds,10)
