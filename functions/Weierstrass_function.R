3242d4f55a0df95c693b44e3b3ac659397dcbd3c
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-09-04 06:28:51 +0200

    Update Weierstrass_function.R

--- functions/Weierstrass_function.R
+++ functions/Weierstrass_function.R
@@ -1,26 +1,38 @@
-
+##########################################
+#
 # Weierstrass function
-
+#
 # 0 < a < 1
 # b odd positive integer
 # ab > 1 + 3/2 pi
+#
+# Series:
+# Little Useless-useful R functions #53
+# Created: September 03, 2023
+# Author: Tomaz Kastrun
+# Blog: tomaztsql.wordpress.com
+# V.1.0
+#
+# Changelog: 
+#
+##########################################
 
 
 
-weierstrass <- function(x, a = 0.5, b = 3) {
-  result <- 0
+weierstrass_curve <- function(x,a,b) {
+  values <- 0
   for (n in 0:100) {  
-    result <- result + (a^n * cos(b^n * pi * x)) }
- return(result)
+    values <- values + (a**n * cos(b**n * pi * x)) }
+ return(values)
 }
 
 len <- 1000
-a <- 0.5  # 0 < a < 1
-b <- 3    # b odd positive integer
-x <- seq(-2, 2, length.out=len)  
-y <- weierstrass(x, a, b)
 
+# x <- seq(-0.4, 0.4, length.out=len)  
+# y <- weierstrass_curve(x,0.5,5)
+x <- seq(-2, 2, length.out=len)
+y <- weierstrass_curve(x,0.3,5)
 
-plot(x, y, type = "l", col = "red", lwd = 1, main = "Weierstrass curve")
+plot(x, y, type = "l", col = "red", main = "Weierstrass curve")
 
 
