9853dc78844f43401f1c94d24c5776afa385199d
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-10-01 20:48:37 +0200

    update

--- functions/multiplication_table.R
+++ functions/multiplication_table.R
@@ -1,25 +1,20 @@
-# multiplication table 10x10
+################
+# multiplication 
+# table 10x10
+################
 
 library(tidyverse)
 
-multiplication_table <- tibble(
-  x = 1:10,
-  y = 1:10
-) %>% 
-  complete(x, y) %>% 
-  mutate(product = x * y)
-
-ggplot(multiplication_table, aes(x, y)) +
+tibble(x=1:10, y = 1:10) %>%
+  complete(x,y) %>%
+  mutate(prod = x*y) %>%
+  ggplot( aes(x, y)) +
   geom_tile(color = "black") +
-  geom_text(aes(label = product), color = "white") +
+  geom_text(aes(label = prod), color = "white") +
   scale_y_continuous(trans = "reverse") +
   coord_fixed() +
   theme_bw() + theme_void()
-#  theme_void()
 
 
 
 
-tibble(x=1:10, y = 1:10) %>%
-  complete(x,y) %>%
-  mutate(prod = x*y)
