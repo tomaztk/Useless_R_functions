bc17ec04c9ffbd86f200cb6dd637bf2bc7de43ef
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-11-15 06:59:36 +0100

    updating

--- functions/R-squared.R
+++ functions/R-squared.R
@@ -36,10 +36,55 @@ r2.0 <- function(sig){
 }
 
 
+# plot shows sinking sigmas (starting with 1) where R2=1 and the model is completely wrong, respectively.
+
 sigmas <- seq(0.5,20,length.out = 20)
 rout <- sapply(sigmas, r2.0)             # apply our function to a series of sigma values
 plot(rout ~ sigmas, type="b")
 
-# plot shows sinking sigmas (starting with 1) where R2=1 and the model is completely wrong, respectively.
 
 
+set.seed(2908)
+x <- rexp(80,rate=0.005)                     # our predictor is data from an exponential distribution
+y <- (x-1)^2 * runif(80, min=0.8, max=1.2)   # non-linear data generation
+plot(x,y)				   
+
+
+# check R squared
+summary(lm(y ~ x))$r.squared
+
+
+
+# R-squared says nothing about prediction error, even with 
+# exactly the same, and no change in the coefficients. 
+# R-squared can be anywhere between 0 and 1 just by changing the range of X. 
+# We're better off using Mean Square Error (MSE) as a measure of prediction error.
+
+set.seed(2908)
+x <- seq(1,10,length.out = 200)
+y <- 2 + 1.2*x + rnorm(200,0,sd = 0.9) #sd = 0.9
+mod1 <- lm(y ~ x)
+summary(mod1)$r.squared
+# [1] 0.9250846
+
+# calculate MSE
+sum((fitted(mod1) - y)^2)/200  
+# [1] 0.7815316
+
+# changing the range
+set.seed(2908)
+
+#getting different R2
+x <- seq(1,3,length.out = 200)
+
+y <- 2 + 1.2*x + rnorm(200,0,sd = 0.9)
+mod1 <- lm(y ~ x)
+summary(mod1)$r.squared
+# [1] 0.3645032
+
+#but MSE is the same
+sum((fitted(mod1) - y)^2)/200  
+# [1] 0.7815316
+
+
+# The R2 falls from 0.92 to 0.36 but the MSE is practically unchanged!
