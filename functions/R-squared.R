461eaecea614d377e6b1a8956fdbf68989faf7bd
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-11-27 21:54:03 +0100

    update

--- functions/R-squared.R
+++ functions/R-squared.R
@@ -29,18 +29,17 @@ all.equal(summary(mod)$r.squared  , mss/tss  )
 # It can be arbitrarily low when the model is completely correct. By making sigma2 large 
 # large, we drive R-squared towards 0, even when every assumption of the simple linear regression model is correct in every particular.
 
-r2.0 <- function(sig){
+useless_r2_with_sigma <- function(sig){
   x <- seq(1,10,length.out = 100)        
   y <- 2 + 1.2*x + rnorm(100,0,sd = sig) #  adding  some random noise to function
   summary(lm(y ~ x))$r.squared            
 }
 
-
 # plot shows sinking sigmas (starting with 1) where R2=1 and the model is completely wrong, respectively.
 
-sigmas <- seq(0.5,20,length.out = 20)
-rout <- sapply(sigmas, r2.0)             # apply our function to a series of sigma values
-plot(rout ~ sigmas, type="b")
+assumption_sigma <- seq(0.5,20,length.out = 100)
+results <- sapply(assumption_sigma, useless_r2_with_sigma)             # apply our function to a series of sigma values
+plot(results ~ assumption_sigma, type="b")
 
 
 
@@ -53,6 +52,7 @@ plot(x,y)
 # check R squared
 summary(lm(y ~ x))$r.squared
 
+### tule gre mal čau...
 # R-squared says nothing about prediction error, even with 
 # exactly the same, and no change in the coefficients. 
 # R-squared can be anywhere between 0 and 1 just by changing the range of X. 
