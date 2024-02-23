43f22ed05bb22fafdcdad257d481a2b18ecc0b55
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-02-23 06:50:07 +0100

    Update Catalan_number.R

--- functions/Catalan_number.R
+++ functions/Catalan_number.R
@@ -2,6 +2,8 @@
 ## Catalan number ###
 #####################
 
+library(ggplot2)
+library(tidyverse)
 
 # Function for factorial
 factorial <- function(n) {
@@ -29,3 +31,26 @@ for (i in 0:10) {
 
 
 # Draw graph
+df <- as.data.frame(NULL)
+for (i in 0:10) {
+  df_i <- print(i)
+  cat_i <- catalan(i)
+  fac_i <- factorial(i)
+  df <- rbind(df, c(df_i, cat_i, fac_i))
+}
+
+colnames(df) <- c("i", "catalan", "factorial")
+
+
+ggplot(df, aes(x=i)) + 
+  geom_line(aes(y = catalan), color = "darkgreen") + 
+  geom_line(aes(y = factorial), color="steelblue") 
+
+# or
+df %>%
+  select(i, catalan, factorial) %>%
+  gather(key = "variable", value = "value", -i) %>%
+  ggplot(aes(x = i, y = value)) + 
+    geom_line(aes(color = variable, linetype = variable)) + 
+    scale_color_manual(values = c("darkgreen", "steelblue"))
+
