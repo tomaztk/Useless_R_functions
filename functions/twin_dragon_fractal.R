4f94183a87e2af5abb57f5d2106dfa72b68c2771
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-06-10 05:15:30 +0200

    update

--- functions/twin_dragon_fractal.R
+++ functions/twin_dragon_fractal.R
@@ -62,3 +62,42 @@ ggplot(twin_dragon_data, aes(x = x, y = y)) +
   coord_equal() +
   theme_void() +
   ggtitle("Twin Dragon Fractal")
+
+
+
+
+
+## Heighway Dragon Curve with 15 iterations
+
+# the iterated functions
+f1 <- function(z) {
+  (1 + 1i) * z / 2
+}
+
+f2 <- function(z) {
+  1 - (1 - 1i) * z / 2
+}
+
+generate_dragon_curve <- function(iterations) {
+  points <- complex(real = 0, imaginary = 0)
+    for (i in 1:iterations) {
+    new_points <- vector("complex", length = length(points) * 2)
+    for (j in 1:length(points)) {
+      new_points[2 * j - 1] <- f1(points[j])
+      new_points[2 * j] <- f2(points[j])
+    }
+    points <- new_points
+  }
+  
+  return(points)
+}
+
+# Plot the Dragon Curve
+plot_dragon_curve <- function(iterations) {
+  points <- generate_dragon_curve(iterations)
+  plot(Re(points), Im(points), type = "p", pch = ".", col = "blue", asp = 1,
+       xlab = "Re", ylab = "Im", main = paste("Heighway Dragon Curve with", iterations, "iterations"))
+}
+
+plot_dragon_curve(15)
+
