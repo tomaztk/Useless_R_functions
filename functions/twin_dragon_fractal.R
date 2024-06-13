7ecfbcf83a998fba0b97181e447c5d4c9e3b69c2
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-06-13 17:30:26 +0200

    update

--- functions/twin_dragon_fractal.R
+++ functions/twin_dragon_fractal.R
@@ -1,73 +1,11 @@
 # twin - dragon Fractal
 # aka david Knuth dragon
+## Heighway Dragon Curve with 15 iterations
 
 
 library(ggplot2)
 
-# Define the function to generate Twin Dragon fractal
-generate_twin_dragon <- function(iterations) {
-  # Initialize points
-  x <- c(0, 1)
-  y <- c(0, 0)
-  colors <- c(1, 2)  
-  
-  for (i in 1:iterations) {
-    # Calculate new points
-    new_x <- c()
-    new_y <- c()
-    new_colors <- c()
-    for (j in 1:(length(x) - 1)) {
-      x1 <- x[j]
-      y1 <- y[j]
-      x2 <- x[j + 1]
-      y2 <- y[j + 1]
-      # Add sin function
-      new_x <- c(new_x, x1, (x1 + x2 - y1 + y2) / 2)
-      new_y <- c(new_y, y1, (y1 + y2 + x1 - x2) / 2)
-      new_colors <- c(new_colors, colors[j], ifelse(colors[j] == 1, 2, 1))
-    }
-    new_x <- c(new_x, tail(x, n = 1))
-    new_y <- c(new_y, tail(y, n = 1))
-    new_colors <- c(new_colors, tail(colors, n = 1))
-    x <- new_x
-    y <- new_y
-    colors <- new_colors
-  }
-  
-  return(data.frame(x, y, color = as.factor(colors)))
-}
 
-# Define the function to plot Twin Dragon fractal
-plot_twin_dragon <- function(data) {
-  ggplot(data, aes(x = x, y = y)) +
-    geom_path() +
-    coord_equal() +
-    theme_void() +
-    ggtitle("Twin Dragon Fractal")
-}
-
-# Generate the Twin Dragon fractal data
-iterations <- 15
-twin_dragon_data <- generate_twin_dragon(iterations)
-
-
-
-# function
-plot_twin_dragon(twin_dragon_data)
-
-
-
-ggplot(twin_dragon_data, aes(x = x, y = y)) +
-  geom_path() +
-  coord_equal() +
-  theme_void() +
-  ggtitle("Twin Dragon Fractal")
-
-
-
-
-
-## Heighway Dragon Curve with 15 iterations
 
 # the iterated functions
 f1 <- function(z) {
@@ -78,27 +16,37 @@ f2 <- function(z) {
   1 - (1 - 1i) * z / 2
 }
 
+# Function to generate points using IFS
 generate_dragon_curve <- function(iterations) {
+  # Initial point
   points <- complex(real = 0, imaginary = 0)
-    for (i in 1:iterations) {
+  colors <- c() # Vector to store colors
+  
+  # Iterate to generate points
+  for (i in 1:iterations) {
     new_points <- vector("complex", length = length(points) * 2)
+    new_colors <- vector("character", length = length(points) * 2)
     for (j in 1:length(points)) {
       new_points[2 * j - 1] <- f1(points[j])
+      new_colors[2 * j - 1] <- ifelse(i %% 2 == 1, "darkblue", "red") # Alternating colors
       new_points[2 * j] <- f2(points[j])
+      new_colors[2 * j] <- ifelse(i %% 2 == 1, "red", "darkblue") # Alternating colors
     }
     points <- new_points
+    colors <- c(colors, new_colors)
   }
   
-  return(points)
+  return(list(points = points, colors = colors))
 }
-
 # Plot the Dragon Curve
 plot_dragon_curve <- function(iterations) {
-  points <- generate_dragon_curve(iterations)
-  plot(Re(points), Im(points), type = "p", pch = ".", col = "blue", asp = 1,
-       xlab = "", ylab = "", main = paste("Heighway Dragon Curve with", iterations, "iterations"), 
-       axes=FALSE, frame.plot=FALSE)
+  result <- generate_dragon_curve(iterations)
+  points <- result$points
+  colors <- result$colors
+  plot(Re(points), Im(points), type = "p", pch = ".", col = colors, asp = 1,
+       xlab = "Re", ylab = "Im", main = paste("Heighway Dragon Curve with", iterations, "iterations"))
 }
+
 
 plot_dragon_curve(15)
 
