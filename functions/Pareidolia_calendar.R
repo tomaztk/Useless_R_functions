89a8788888f57b28dd9190633226b23bfd0f2ae0
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-01-04 21:20:58 +0100

    update

--- functions/Pareidolia_calendar.R
+++ functions/Pareidolia_calendar.R
@@ -21,6 +21,33 @@ library(lubridate)
 ###########################################
 
 
+days_in_month <- function(date) {
+  return(as.numeric(format(date + months(1) - days(1), "%d")))
+}
+
+# 
+# year <- 2025
+# month <- 2
+# as.Date(paste(year, month, "01", sep = "-"))
+# as.Date(paste(year, month, days_in_month(as.Date(paste(year, month, "01", sep = "-"))), sep = "-"))
+# 
+# 
+# days <- seq.Date(from = as.Date(paste(year, month, "01", sep = "-")),
+#                  to = as.Date(paste(year, month, days_in_month(as.Date(paste(year, month, "01", sep = "-"))), sep = "-")),
+#                  by = "day")
+# 
+# calendar <- data.frame(
+#   date = days,
+#   day = wday(days, label = TRUE, locale = Sys.getlocale("LC_TIME") ),
+#   #week = as.numeric(format(days, "%U")) - min(as.numeric(format(days, "%U"))) + 1
+#   week = lubridate::week(days)
+# )
+# 
+# calendar
+# lubridate::week("2025-02-28")
+# 
+# wday(x, label = FALSE, abbr = TRUE, week_start = getOption("lubridate.week.start", 7), locale = Sys.getlocale("LC_TIME"))
+
 calendar_pareidolia <- function(year, month) {
 
   days <- seq.Date(from = as.Date(paste(year, month, "01", sep = "-")),
@@ -29,14 +56,16 @@ calendar_pareidolia <- function(year, month) {
   calendar <- data.frame(
     date = days,
     day = wday(days, label = TRUE),
-    week = as.numeric(format(days, "%U")) - min(as.numeric(format(days, "%U"))) + 1
+    week = as.numeric(format(days, "%U")) - min(as.numeric(format(days, "%U"))) + 1,
+    week_graph = lubridate::week(days)
   )
+  head(calendar,3)
   
   # Define quazi-patterns (example: "eyes and mouth" for faces)
   patterns <- list(
-    face = c("Su", "Mo", "We", "Fr", "Su"),
-    smile = c("Tu", "We", "Th"),
-    vertical_line = c("Mo", "Tu", "We", "Th", "Fr")
+    face = c("Sun", "Mon", "Wed", "Fri", "Sun"),
+    smile = c("Tue", "Wed", "Thu", "Sat"),
+    vertical_line = c("Mon", "Tue", "Wed", "Thu", "Fri")
   )
   
   # "detect" quazi- patterns
@@ -52,10 +81,10 @@ calendar_pareidolia <- function(year, month) {
   })
   
   detected_patterns <- bind_rows(detected_patterns, .id = "pattern_name")
-  
+  View(detected_patterns)
   
   ## Draw calendar
-  ggplot(calendar, aes(x = day, y = week)) +
+  ggplot(calendar, aes(x = day, y = week_graph)) +
     geom_tile(aes(fill = date), color = "white", show.legend = FALSE) +
     scale_fill_gradient(low = "lightblue", high = "blue") +
     geom_text(aes(label = day(date)), size = 4, color = "black") +
@@ -67,7 +96,7 @@ calendar_pareidolia <- function(year, month) {
     theme_minimal() +
     labs(
       title = paste("Calendar Pareidolia for", month, year),
-      subtitle = "Highlighted patterns are ""detected"" pareidolia-like shapes",
+      subtitle = "Highlighted patterns are 'detected' pareidolia-like shapes",
       x = "Day of the Week",
       y = "Week?"
     )
@@ -78,6 +107,6 @@ days_in_month <- function(date) {
   return(as.numeric(format(date + months(1) - days(1), "%d")))
 }
 
-
 # Run the function for February 2025
-calendar_pareidolia(2025, 2) 
+calendar_pareidolia(2025, 1) 
+
