18a36e0c7710b8452054a725a6c694d143c22269
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-04-07 20:12:12 +0200

    adding changes

--- figures/fermi_calculator.R
+++ figures/fermi_calculator.R
@@ -18,15 +18,15 @@
 library(ggplot2)
 library(dplyr)
 
-fermi_calculator <- function(problem = "piano_tuners", simulations = 1000) {
+fermi_calculator <- function(problem = "piano_tuners_chicago", simulations = 1000) {
   
-  if (problem == "piano_tuners") {
+  if (problem == "piano_tuners_chicago") {
     
-    pop_chicago <- rnorm(simulations, mean = 2.7e6, sd = 2e5)  # Population of Chicago
-    households <- pop_chicago / rnorm(simulations, mean = 2.5, sd = 0.2)  # People per household
-    pianos_per_household <- rnorm(simulations, mean = 0.02, sd = 0.005)  # Fraction of households with pianos
-    pianos_tuned_per_year <- rnorm(simulations, mean = 1, sd = 0.2)  # How often a piano gets tuned per year
-    tunings_per_tuner <- rnorm(simulations, mean = 1000, sd = 200)  # Tunings per tuner per year
+    pop_chicago <- rnorm(simulations, mean = 2.7e6, sd = 2e5) 
+    households <- pop_chicago / rnorm(simulations, mean = 2.5, sd = 0.2)  # ppl per house
+    pianos_per_household <- rnorm(simulations, mean = 0.02, sd = 0.005)  # fraction of pianos per house
+    pianos_tuned_per_year <- rnorm(simulations, mean = 1, sd = 0.2)  # freq of piano tunning
+    tunings_per_tuner <- rnorm(simulations, mean = 1000, sd = 200)  
     
     num_pianos <- households * pianos_per_household
     total_tunings <- num_pianos * pianos_tuned_per_year
@@ -48,18 +48,15 @@ fermi_calculator <- function(problem = "piano_tuners", simulations = 1000) {
     return(summary(num_tuners))
   }
   
-  else if (problem == "coffee_consumption") {
-    # Assumptions for coffee in Ljubljana
-    pop_lj <- rnorm(simulations, mean = 3.6e5, sd = 2e4)  # Population of Ljubljana
+  else if (problem == "coffee_consumption_ljubljana") {
+    pop_lj <- rnorm(simulations, mean = 3.6e5, sd = 2e4)  # citizens of LJ
     fraction_coffee_drinkers <- rnorm(simulations, mean = 0.65, sd = 0.05)  
     cups_per_day <- rnorm(simulations, mean = 2.5, sd = 0.5)  
     cup_size_liters <- rnorm(simulations, mean = 0.18, sd = 0.02)  # Size of a cup in liters
     
-   
     daily_coffee_liters <- pop_lj * fraction_coffee_drinkers * cups_per_day * cup_size_liters
     data <- data.frame(DailyCoffeeLiters = daily_coffee_liters)
     
-    # Plot the estimated coffee consumption
     p <- ggplot(data, aes(x = DailyCoffeeLiters)) +
       geom_histogram(fill = "brown", color = "black", bins = 30, alpha = 0.7) +
       geom_vline(xintercept = mean(daily_coffee_liters), color = "red", linetype = "dashed") +
@@ -74,12 +71,16 @@ fermi_calculator <- function(problem = "piano_tuners", simulations = 1000) {
   }
   
   else {
-    stop("Problem not recognized. Try 'piano_tuners' or 'coffee_consumption'.")
+    stop("Problem not recognized. Try 'piano_tuners_chicago' or 'coffee_consumption_ljubljana'.")
   }
 }
 
 
-# function usage:
+#################
+# function usage
+#################
 
-fermi_calculator("piano_tuners")
-fermi_calculator("coffee_consumption")
+fermi_calculator("piano_tuners_chicago")
+fermi_calculator("coffee_consumption_ljubljana")
+
+fermi_calculator("drunks_in_dublin")
