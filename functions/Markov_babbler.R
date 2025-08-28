f07dbcb209112bb04469f0893b13de5386b43e86
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-08-28 19:53:28 +0200

    update

--- functions/Markov_babbler.R
+++ functions/Markov_babbler.R
@@ -158,6 +158,8 @@ cat(result$text)
 print(result$plot)
 
 
+#sample of Grimm story (in Slovenian Language)
+
 text <- "Brata Grimm: RDEČA KAPICA
 Nekoč je živela majhna deklica. Bila je tako zelo ljubka, da jo je vsak že na prvi pogled vzljubil. Izmed
 vseh ljudi na tem svetu pa jo je imela najraje njena babica. Ni je bilo stvari, ki je ne bi naredila zanjo.
