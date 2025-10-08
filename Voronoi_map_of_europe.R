071e164cd321eb3f771efe16272b7f3517554062
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-10-08 08:30:47 +0200

    Update Voronoi_map_of_europe.R

--- Voronoi_map_of_europe.R
+++ Voronoi_map_of_europe.R
@@ -102,3 +102,168 @@ ggplot() +
   theme_minimal(base_size = 11) +
   theme(panel.grid.major = element_blank(), axis.title = element_blank())
 
+
+##### Comparison
+
+
+
+library(sf)
+library(lwgeom)
+library(dplyr)
+library(ggplot2)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(cartogram)
+
+
+# EU map
+world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
+eu <- world |>
+  dplyr::filter(continent == "Europe" | name_long %in% c("Russia","Turkey","Kazakhstan","Azerbaijan","Cyprus")) |>
+  st_make_valid()
+
+
+eu <- world |>
+  dplyr::filter(continent == "Europe" | name_long %in% c("Turkey","Cyprus")) |>
+  dplyr::filter(sovereignt != "Russia" &  sovereignt != "Turkey" ) |>
+  st_make_valid()
+
+eu_ae <- st_transform(eu, 3035)  # ETRS89 / LAEA Europe / check ----->
+
+
+
+# some metal data :)
+metal_weight <- tibble::tribble(
+  ~iso_a2, ~bands,
+  "FI", 1650,
+  "SE", 1000,
+  "NO", 600,
+  "IS",  450,
+  "DK", 700,
+  "EE",  500,
+  "LV",  380,
+  "LT",  520
+)
+
+# Normal state
+metal_no <- tibble::tribble(
+  ~iso_a2, ~bands,
+  "FI", 1,
+  "SE", 1,
+  "NO", 1,
+  "IS",  1,
+  "DK", 1,
+  "EE",  1,
+  "LV",  1,
+  "LT",  1
+)
+
+eu_dat <- eu_ae |>
+  left_join(metal_weight, by = "iso_a2")
+
+
+eu_dat_nometal <- eu_ae |>
+  left_join(metal_no, by = "iso_a2")
+
+
+
+# add random noise weights :)
+if (anyNA(eu_dat$bands)) {
+  min_pos <- max(1, floor(min(eu_dat$bands, na.rm = TRUE) * 0.05))
+  eu_dat$bands[is.na(eu_dat$bands)] <- min_pos
+}
+
+# add nothing - no  weights :)
+if (anyNA(eu_dat_nometal$bands)) {
+  min_pos_nometal <- max(1, floor(min(eu_dat_nometal$bands, na.rm = TRUE) * 0.05))
+  eu_dat_nometal$bands[is.na(eu_dat_nometal$bands)] <- min_pos_nometal
+}
+
+
+# Continuous cartogram  - add weithgt of the bands :D
+eu_cart <- cartogram::cartogram_cont(eu_dat, weight = "bands", itermax = 15)
+eu_cart_nometal <- cartogram::cartogram_cont(eu_dat_nometal, weight = "bands", itermax = 15)
+
+# Voronoi tessellation in the DEFORMED space ----
+seeds <- st_point_on_surface(eu_cart)
+seeds_nometal <- st_point_on_surface(eu_cart_nometal)
+
+win <- st_make_valid(st_union(eu_cart))
+win_nometal <- st_make_valid(st_union(eu_cart_nometal))
+
+v_raw <- st_voronoi(st_union(st_geometry(seeds)))
+v_raw_nometal <- st_voronoi(st_union(st_geometry(seeds_nometal)))                  
+
+vor  <- st_collection_extract(v_raw, "POLYGON") |> st_sf(crs = st_crs(eu_cart))
+vor_nometal  <- st_collection_extract(v_raw_nometal, "POLYGON") |> st_sf(crs = st_crs(eu_cart_nometal))
+
+vor  <- suppressWarnings(st_intersection(vor, win))
+vor_nometal  <- suppressWarnings(st_intersection(vor_nometal, win_nometal))                
+
+
+# add seeds for voronoi diagram
+cent <- st_centroid(vor)
+idx  <- st_nearest_feature(cent, seeds)
+vor$iso_a2    <- seeds$iso_a2[idx]
+vor$name_long <- seeds$name_long[idx]
+vor$bands     <- seeds$bands[idx]
+
+
+# add seeds for voronoi diagram _ no metal
+cent_nometal <- st_centroid(vor_nometal)
+idx_nometal  <- st_nearest_feature(cent_nometal, seeds_nometal)
+vor_nometal$iso_a2    <- seeds_nometal$iso_a2[idx]
+vor_nometal$name_long <- seeds_nometal$name_long[idx]
+vor_nometal$bands     <- seeds_nometal$bands[idx]
+
+
+
+ggplot() +
+  geom_sf(data = vor, aes(fill = bands), color = "grey20", linewidth = 0.2) +
+  geom_sf(data = win, fill = NA, color = "black", linewidth = 0.4) +
+  scale_fill_viridis_c(option = "C", name = "Metal bands") +
+  coord_sf(crs = st_crs(3035)) +
+  theme_minimal(base_size = 11) +
+  theme(panel.grid.major = element_blank(), axis.title = element_blank())
+
+
+
+ggplot() +
+  geom_sf(data = vor_nometal, aes(fill = bands), color = "grey20", linewidth = 0.2) +
+  geom_sf(data = win_nometal, fill = NA, color = "black", linewidth = 0.4) +
+  scale_fill_viridis_c(option = "C", name = "No bands :)") +
+  coord_sf(crs = st_crs(3035)) +
+  theme_minimal(base_size = 11) +
+  theme(panel.grid.major = element_blank(), axis.title = element_blank())
+
+
+
+### 
+library(patchwork)
+
+graph_left <-  ggplot() +
+  geom_sf(data = vor_nometal, aes(fill = bands), color = "grey20", linewidth = 0.2) +
+  geom_sf(data = win_nometal, fill = NA, color = "black", linewidth = 0.4) +
+  scale_fill_viridis_c(option = "C", name = "No bands :)") +
+  coord_sf(crs = st_crs(3035)) +
+  theme_minimal(base_size = 11) +
+  theme(panel.grid.major = element_blank(), axis.title = element_blank())
+
+  
+graph_right <-  ggplot() +
+  geom_sf(data = vor, aes(fill = bands), color = "grey20", linewidth = 0.2) +
+  geom_sf(data = win, fill = NA, color = "black", linewidth = 0.4) +
+  scale_fill_viridis_c(option = "C", name = "Metal bands") +
+  coord_sf(crs = st_crs(3035)) +
+  theme_minimal(base_size = 11) +
+  theme(panel.grid.major = element_blank(), axis.title = element_blank())
+
+  
+
+  
+combined_plot <- graph_left + graph_right +  
+  plot_annotation(title = "Europe metal map combination")
+
+print(combined_plot)
+
+
